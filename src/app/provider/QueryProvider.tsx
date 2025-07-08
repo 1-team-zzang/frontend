09ea3974b98f20c0db275b93e5d2e9f865bab3f7
D5ng <121039744+D5ng@users.noreply.github.com>
2025-07-08 15:47:49 +0900

    chore: setup library (#18)
    * chore: init tailwind css
    
    * chore: add cva, clsx, tw-merge
    
    * feat: add utility function for class name merging
    
    * chore: update VSCode settings to include Tailwind CSS class functions and remove unused .gitkeep file
    
    * chore: add react-error-boundary
    
    * chore: add @tanstack/react-query
    
    * chore: add @hookform/resolvers, react-hook-form,  zod
    
    * chore: add date-fns
    
    * chore: add testing libraries and update package scripts
    
    * chore: add Vitest configuration and setup file for testing
    
    * refactor: remove StrictMode wrapper from App rendering
    
    * chore: add react-router
    
    * feat: React Router provider and routing setup
    
    * feat: add QueryProvider, query options
    
    * feat: integrate QueryProvider and ReactRouterProvider in App component
    
    * feat: add @tanstack/react-query-devtools

---
+++ src/app/provider/QueryProvider.tsx
@@ -0,0 +1,31 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+
+import type { ReactNode } from 'react'
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: 0,
+      staleTime: 1000 * 60 * 5,
+      gcTime: 1000 * 60 * 10,
+      refetchOnWindowFocus: false,
+      refetchOnMount: false,
+      refetchOnReconnect: false,
+      throwOnError: true,
+    },
+    mutations: {
+      retry: 0,
+      throwOnError: true,
+    },
+  },
+})
+
+export default function QueryProvider({ children }: { children: ReactNode }) {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools />
+      {children}
+    </QueryClientProvider>
+  )
+}
