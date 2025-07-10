5e0809318ae5c7d88fc57e8a35a36f38bc4e8c2d
d5ng <dong5ffice@gmail.com>
2025-07-10 16:03:27 +0900

    test: refactor Slot Component test

--- src/shared/utils/Slot.test.tsx
+++ src/shared/utils/Slot.test.tsx
@@ -1,6 +1,6 @@
-import { render, screen } from '@testing-library/react'
+import { render, screen, cleanup } from '@testing-library/react'
 import { Link, MemoryRouter } from 'react-router'
-import { beforeEach, describe, expect, it } from 'vitest'
+import { describe, expect, it, afterEach, beforeEach } from 'vitest'
 
 import { Slot } from './Slot'
 import { Slottable } from './Slottable'
@@ -12,34 +12,41 @@ export default function Button({
   children,
   ...restProps
 }: ButtonHTMLAttributes<HTMLButtonElement> & { asChild?: boolean }) {
-  const Component = asChild ? Slot : 'button'
+  const Element = asChild ? Slot : 'button'
 
   return (
-    <Component {...restProps}>
-      <Link to="/">{children}</Link>
-    </Component>
+    <Element {...restProps}>
+      <Slottable>{children}</Slottable>
+    </Element>
   )
 }
 
 describe('Slot', () => {
-  it('Slot 컴포넌트는 자식 태그를 그대로 렌더링 한다', () => {
+  afterEach(cleanup)
+
+  describe('Slot 컴포넌트는 Slottable을 통해 자식 태그를 그대로 렌더링 한다', () => {
     beforeEach(() => {
       render(
         <MemoryRouter>
           <Slot>
-            <Link to="/test">Test Link</Link>
+            <Slottable>
+              <Link to="/test">Test Link</Link>
+            </Slottable>
           </Slot>
         </MemoryRouter>,
       )
+    })
 
-      const link = screen.getByRole('link')
+    it('Slottable을 사용하여 Link를 렌더링', () => {
+      const link = screen.getByRole('link', { name: 'Test Link' })
       expect(link).toBeInTheDocument()
       expect(link.tagName).toBe('A')
       expect(link).toHaveAttribute('href', '/test')
+      expect(link).toHaveTextContent('Test Link')
     })
   })
 
-  it('asChild 옵션을 사용했을 때 Link 태그가 제대로 렌더링되는지 테스트', () => {
+  describe('asChild 옵션을 사용했을 때 Link 태그가 제대로 렌더링되는지 테스트', () => {
     beforeEach(() => {
       render(
         <MemoryRouter>
@@ -48,7 +55,9 @@ describe('Slot', () => {
           </Button>
         </MemoryRouter>,
       )
+    })
 
+    it('Link 태그가 제대로 렌더링되는지 테스트', () => {
       const anchorElement = screen.getByRole('link')
       const buttonElement = screen.queryByRole('button')
 
@@ -59,34 +68,32 @@ describe('Slot', () => {
     })
   })
 
-  it('Slottable을 이용한 일부분 위임 테스트', () => {
+  describe('Slottable을 사용하여 일부분 위임 테스트', () => {
     beforeEach(() => {
       render(
         <Slot>
-          <div>오른쪽 컴포넌트</div>
-          <Slottable>
-            <span>Slottable</span>
-          </Slottable>
           <div>왼쪽 컴포넌트</div>
+          <Slottable>
+            <button type="button">
+              <span>Slottable</span>
+            </button>
+          </Slottable>
+          <div>오른쪽 컴포넌트</div>
         </Slot>,
       )
+    })
 
+    it('Slottable을 사용하여 일부분 위임 테스트', () => {
       const slottable = screen.getByText('Slottable')
       const leftComponent = screen.getByText('왼쪽 컴포넌트')
       const rightComponent = screen.getByText('오른쪽 컴포넌트')
+      const button = screen.getByRole('button')
 
-      expect(slottable).toBeInTheDocument()
-      expect(leftComponent).toBeInTheDocument()
-      expect(rightComponent).toBeInTheDocument()
+      const slotChildren = Array.from(button?.childNodes || []).filter((node) => node.nodeType === 1)
 
-      expect(slottable.tagName).toBe('SPAN')
-
-      const parent = slottable.parentElement!
-      const children = Array.from(parent.childNodes).filter((node) => node.nodeType === 1)
-
-      expect(children[0]).toBe(rightComponent)
-      expect(children[1]).toBe(slottable)
-      expect(children[2]).toBe(leftComponent)
+      expect(slotChildren[0]).toBe(leftComponent)
+      expect(slotChildren[1]).toBe(slottable)
+      expect(slotChildren[2]).toBe(rightComponent)
     })
   })
 })
