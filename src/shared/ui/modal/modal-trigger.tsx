6eb674fab347615b1dc67fb85f5e64d2f7bdb9a9
d5ng <dong5ffice@gmail.com>
2025-07-14 01:38:29 +0900

    fix: 트리거 컴포넌트에 Slot 기능 추가

--- src/shared/ui/modal/modal-trigger.tsx
+++ src/shared/ui/modal/modal-trigger.tsx
@@ -1,15 +1,22 @@
-import { cn } from '@/shared/utils'
+import { cn, Slot } from '@/shared/utils'
 
 import { useModalContext } from './modal-context'
 
 import type { ButtonHTMLAttributes } from 'react'
 
+interface Props extends ButtonHTMLAttributes<HTMLButtonElement> {
+  asChild?: boolean
+}
+
 // Todo: button에 ui 컴포넌트로 대체해야함. (아직 버튼 UI가 없어서 작업 X)
-export default function ModalTrigger({ children, className, ...restProps }: ButtonHTMLAttributes<HTMLButtonElement>) {
+export default function ModalTrigger({ children, className, asChild, ...restProps }: Props) {
   const { onOpenChange } = useModalContext()
+
+  const Element = asChild ? Slot : 'button'
+
   return (
-    <button className={cn('', className)} onClick={() => onOpenChange(true)} {...restProps}>
+    <Element className={cn('', className)} onClick={() => onOpenChange(true)} {...restProps}>
       {children}
-    </button>
+    </Element>
   )
 }
