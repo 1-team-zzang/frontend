22abf9e725334e6c956f2b39d88e4074174bc018
mxkxx <mxkxx@naver.com>
2025-07-14 23:32:46 +0900

    feat: tooltip message 컴포넌트 구현 및 클래스 추가

---
+++ src/shared/ui/tooltip/tooltip-message.tsx
@@ -0,0 +1,27 @@
+import { cn } from '@/shared/utils/cn'
+
+import { useTooltipContext } from './tooltip-context'
+
+import type { HTMLAttributes } from 'react'
+
+export default function TooltipMessage({ children, className }: HTMLAttributes<HTMLDivElement>) {
+  const { isShow } = useTooltipContext()
+
+  if (!isShow) {
+    return null
+  }
+
+  return (
+    <div className="absolute -right-9 top-10">
+      <div
+        className={cn(
+          'relative bg-gray-95 rounded-lg p-2.5 w-fit tooltip whitespace-nowrap',
+          'after:block after:absolute  after:w-0 after:z-10  after:border-solid after:top-[-13px] after:left-[75%]',
+          className,
+        )}
+      >
+        <span className="text-gray-5 font-normal text-sm">{children}</span>
+      </div>
+    </div>
+  )
+}
