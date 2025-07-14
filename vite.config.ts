43caf56d4a6ea6fccfbbe0200fcdf470bb83e2ff
최민경 <mxkxx@naver.com>
2025-07-14 11:35:37 +0900

    chore: color 수정 및 svg 파일 추가 (#47)
    * chore: color 변수 설정 및 테일윈드 컬러 수정
    
    * chore: svg 파일 추가
    
    * fix: css 컬러 변수 오타 수정
    
    * chore: vite-plugin-svgr
    설치
    
    * chore: vite svgr 설정
    
    * chore: icon 파일 위치 변경
    
    * chore: svg 통합 export 파일 추가
    
    * chore: yellow 컬러갑 수정

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import react from '@vitejs/plugin-react'
+import svgr from 'vite-plugin-svgr'
 import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), svgr({ include: '**/*.svg?react' })],
   test: {
     environment: 'jsdom',
     setupFiles: ['./setup.test.tsx'],
