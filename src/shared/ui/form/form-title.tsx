cd5cb5a95e480436dcaf37a5aa43ebf222e78e60
mxkxx <mxkxx@naver.com>
2025-07-14 11:17:40 +0900

    refactor: form title 타입 수정

--- src/shared/ui/form/form-title.tsx
+++ src/shared/ui/form/form-title.tsx
@@ -1,12 +1,7 @@
 import { cn } from '../../utils'
 
-import type { ReactNode } from 'react'
+import type { HTMLAttributes } from 'react'
 
-interface Props {
-  children: ReactNode
-  className?: string
-}
-
-export default function FormTitle({ children, className }: Props) {
+export default function FormTitle({ children, className }: HTMLAttributes<HTMLHeadingElement>) {
   return <h2 className={cn('text-gray-80 text-xl font-semibold text-center', className)}>{children}</h2>
 }
