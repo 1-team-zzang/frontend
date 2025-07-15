c810d5be8a613f08e40d810d2ea287f8a42ecc95
mxkxx <mxkxx@naver.com>
2025-07-15 13:29:31 +0900

    refactor: 툴팁 메시지 prop을 통해 위치 설정 가능하게 변경

--- src/shared/ui/tooltip/tooltip-message.tsx
+++ src/shared/ui/tooltip/tooltip-message.tsx
@@ -1,10 +1,60 @@
+import { cva, type VariantProps } from 'class-variance-authority'
+
 import { cn } from '@/shared/utils/cn'
 
 import { useTooltipContext } from './tooltip-context'
 
 import type { HTMLAttributes } from 'react'
 
-export default function TooltipMessage({ children, className }: HTMLAttributes<HTMLDivElement>) {
+const positionVariants = cva('', {
+  variants: {
+    position: {
+      top: 'bottom-10',
+      bottom: 'top-10',
+    },
+    arrowAlign: {
+      left: '-left-5',
+      center: 'left-1/2 -translate-x-1/2',
+      right: '-right-5',
+    },
+  },
+  defaultVariants: {
+    position: 'bottom',
+    arrowAlign: 'right',
+  },
+})
+
+const arrowPositionVariants = cva('', {
+  variants: {
+    arrowPosition: {
+      top: 'after:top-[-13px] tooltip-top',
+      bottom: 'after:bottom-[-13px] tooltip-bottom',
+    },
+    arrowAlign: {
+      left: 'after:left-[15px]',
+      center: 'after:left-1/2 after:-translate-x-1/2',
+      right: 'after:right-[15px]',
+    },
+  },
+  defaultVariants: {
+    arrowPosition: 'top',
+    arrowAlign: 'right',
+  },
+})
+
+interface Props
+  extends HTMLAttributes<HTMLDivElement>,
+    VariantProps<typeof positionVariants>,
+    VariantProps<typeof arrowPositionVariants> {}
+
+export default function TooltipMessage({
+  children,
+  className,
+  position,
+  arrowPosition,
+  arrowAlign,
+  ...restProps
+}: Props) {
   const { isShow } = useTooltipContext()
 
   if (!isShow) {
@@ -12,11 +62,12 @@ export default function TooltipMessage({ children, className }: HTMLAttributes<H
   }
 
   return (
-    <div className="absolute -right-5 top-10">
+    <div className={cn('absolute', positionVariants({ position, arrowAlign }))} {...restProps}>
       <div
         className={cn(
-          'relative bg-gray-95 rounded-lg p-2.5 w-fit tooltip whitespace-nowrap',
-          'after:block after:absolute  after:w-0 after:z-10  after:border-solid after:top-[-13px] after:right-[15px]',
+          'relative bg-gray-95 rounded-lg p-2.5 w-fit whitespace-nowrap',
+          'after:block after:absolute after:w-0 after:z-10 after:border-solid',
+          arrowPositionVariants({ arrowPosition, arrowAlign }),
           className,
         )}
       >
