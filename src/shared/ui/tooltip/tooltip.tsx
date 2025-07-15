27ac46388018f607bbc1baf3b686f0073d3ad04b
mxkxx <mxkxx@naver.com>
2025-07-15 09:37:45 +0900

    docs: tooltip 컴포넌트 JSDoc 주석 작성

--- src/shared/ui/tooltip/tooltip.tsx
+++ src/shared/ui/tooltip/tooltip.tsx
@@ -4,6 +4,22 @@ import { cn } from '@/shared/utils'
 
 import { TooltipProvider } from './tooltip-context'
 
+/**
+ * Tooltip 컴포넌트
+ *
+ * @param {import('react').HTMLAttributes<HTMLDivElement>} props - div의 기본 속성들 (className, children 등)
+ * @param {React.ReactNode} props.children - TooltipTrigger, TooltipMessage 등 하위 컴포넌트들
+ * @param {string} [props.className] - 추가로 적용할 CSS 클래스 이름
+ * @returns {JSX.Element} Tooltip 상태를 Context로 감싸 렌더링하는 div
+ *
+ * @example
+ * <Tooltip>
+ *  <TooltipTrigger>
+ *    <IconClose/>
+ *  </TooltipTrigger>
+ *  <TooltipMessage>툴팁 메시지입니다</TooltipMessage>
+ * </Tooltip>
+ */
 export default function Tooltip({ children, className }: HTMLAttributes<HTMLDivElement>) {
   const [isShow, setIsShow] = useState<boolean>(false)
 
