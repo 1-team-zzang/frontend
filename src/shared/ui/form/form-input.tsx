11e28e2bb619f81ab382a1b769d9a9b4ca8fcd6a
mxkxx <mxkxx@naver.com>
2025-07-16 15:29:16 +0900

    refactor: form field 내부와 외부 구분하여 name prop 적용

--- src/shared/ui/form/form-input.tsx
+++ src/shared/ui/form/form-input.tsx
@@ -1,19 +1,20 @@
 import { useFormContext } from 'react-hook-form'
 
-import { cn } from '../../utils/cn'
+import { cn } from '@/shared//utils/cn'
+
+import { useFormFieldContext } from './form-field-context'
 
 import type { InputHTMLAttributes } from 'react'
 
 interface Props extends InputHTMLAttributes<HTMLInputElement> {
-  name: string
+  name?: string
 }
 
-// NOTE: 기본 Input 컴포넌트는 useForm에 종속시키지 않기 위해 register를 prop으로 받는다하셔서
-// 아예 useForm에 종속적인 Input인 FormInput 컴포넌트도 있는게 좋다고 생각해서 만든건데
-// 불필요한 prop drilling을 발생시키는 걸까요?
-
-export default function FormInput({ name, className }: Props) {
+export default function FormInput({ className, name: propName = '' }: Props) {
   const { register } = useFormContext()
-  //   TODO 임시 input / input 컴포넌트 변경하기
-  return <input className={cn('', className)} {...register(name)} name={name} id={name} />
+  const FieldContext = useFormFieldContext()
+
+  const name = FieldContext ? FieldContext.name : (propName as string)
+
+  return <input className={cn('', className)} {...register(name)} id={name} />
 }
