55fd5101f13351dc4d2d898e246d23f343b2cca5
mxkxx <mxkxx@naver.com>
2025-07-16 15:33:57 +0900

    refactor: label 컴포넌트 수정

--- src/shared/ui/form/form-label.tsx
+++ src/shared/ui/form/form-label.tsx
@@ -1,14 +1,23 @@
-import { cn } from '../../utils'
+import { cn } from '@/shared/utils'
+
+import Text from '../text/text'
+
+import { useFormFieldContext, type FormFieldContextValue } from './form-field-context'
 
 import type { LabelHTMLAttributes } from 'react'
 
-interface Props extends LabelHTMLAttributes<HTMLLabelElement> {
-  name: string
-}
-export default function FormLabel({ name, children, className, ...restProps }: Props) {
+export default function FormLabel({ children, className, ...restProps }: LabelHTMLAttributes<HTMLLabelElement>) {
+  const { name } = useFormFieldContext() as FormFieldContextValue
+
   return (
-    <label htmlFor={name} className={cn('text-gray-90 text-sm font-semibold', className)} {...restProps}>
+    <Text
+      as="label"
+      typography="label"
+      htmlFor={name}
+      className={cn('text-gray-90 font-semibold', className)}
+      {...restProps}
+    >
       {children}
-    </label>
+    </Text>
   )
 }
