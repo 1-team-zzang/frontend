feafff6bdea828e0dc4c4a8054d31025f2bb3dd6
yujin <kingyujin56@gmail.com>
2025-07-16 14:49:36 +0900

    Update App.tsx

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,17 +1,9 @@
-import { Calendar, CalendarHeader, CalendarScrollWrapper } from '@/shared/ui/calendar'
-import WeekdayHeader from '@/shared/ui/calendar/weekday-header'
-
 import QueryProvider from './provider/QueryProvider'
 import ReactRouterProvider from './provider/ReactRouterProvider'
 
 export default function App() {
   return (
     <QueryProvider>
-      <Calendar>
-        <CalendarHeader />
-        <WeekdayHeader />
-        <CalendarScrollWrapper />
-      </Calendar>
       <ReactRouterProvider />
     </QueryProvider>
   )
