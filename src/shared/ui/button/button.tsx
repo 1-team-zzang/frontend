c509bd916908270cd99ef867f11318238b3c5fb8
inbae <inbae@appleui-MacBookAir.local>
2025-07-17 14:35:05 +0900

    feat: disabeld 기본값 삭제

--- src/shared/ui/button/button.tsx
+++ src/shared/ui/button/button.tsx
@@ -6,7 +6,7 @@ import type { ButtonHTMLAttributes } from 'react'
 
 interface Props extends ButtonHTMLAttributes<HTMLButtonElement>, Omit<ButtonVariantProps, 'disabled'> {}
 
-export default function Button({ intent, disabled = false, children, className, ...restProps
+export default function Button({ intent, disabled, children, className, ...restProps
 }: Props) {
   return (
     <button className={cn(buttonVariants({ intent, disabled }), className,
