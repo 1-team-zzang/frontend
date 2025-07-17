e954d6fac4cd2ed970169006aaae52eb31509b7b
yujin <kingyujin56@gmail.com>
2025-07-17 19:01:50 +0900

    feat: checkbox 컴포넌트 구현

---
+++ src/shared/ui/checkbox/checkbox.tsx
@@ -0,0 +1,45 @@
+import { cva, type VariantProps } from 'class-variance-authority'
+
+import Text from '../text/text'
+
+import type { ChangeEvent, InputHTMLAttributes } from 'react'
+
+/**
+ * @param onCheckedChange 체크박스의 상태가 변경될 때 호출되는 함수
+ * @param textLabel 체크박스 옆에 표시될 텍스트
+ * @param checkboxId textLabel 있을시 input과 연결
+ *
+ */
+
+interface Props extends InputHTMLAttributes<HTMLInputElement>, VariantProps<typeof checkboxVariants> {
+  onCheckedChange?: (e: ChangeEvent<HTMLInputElement>) => void
+  textLabel?: string
+  checkboxId?: string
+}
+
+const checkboxVariants = cva('cursor-pointer', {
+  variants: {},
+})
+
+export default function Checkbox({ onCheckedChange, textLabel, checkboxId, ...restprops }: Props) {
+  return (
+    <div className="flex">
+      <input
+        id={checkboxId}
+        type="checkbox"
+        onChange={(e) => {
+          if (onCheckedChange) {
+            onCheckedChange(e)
+          }
+        }}
+        className={checkboxVariants()}
+        {...restprops}
+      />
+      {textLabel && (
+        <label htmlFor={checkboxId}>
+          <Text as="span">{textLabel}</Text>
+        </label>
+      )}
+    </div>
+  )
+}
