03474650d95027d3d8108166451382071821f5d1
yujin <kingyujin56@gmail.com>
2025-07-18 09:34:38 +0900

    feat: 프로필부분 사이즈 수정

--- src/shared/ui/gnb/sidebar-menu-header.tsx
+++ src/shared/ui/gnb/sidebar-menu-header.tsx
@@ -5,8 +5,11 @@ import Text from '../text/text'
 import { useGNBContext } from './gnb-conext'
 
 /**
+
  *  sidebar의 상단 프로필, 이름, 닫기 부분
+
  *  프로필 공통컴포넌트 완성되면 수정예정
+
  */
 
 interface Prop {
@@ -15,14 +18,17 @@ interface Prop {
 
 export default function SideBarMenuHeader({ username }: Prop) {
   const { handleCloseSidebar } = useGNBContext()
+
   return (
     <div className="w-full h-14 px-4 flex items-center justify-between border-b border-gray-5">
       <div className="flex items-center justify-center gap-3">
-        <div className="w-10 h-10 rounded-full bg-gray-5" />
+        <div className="size-10 rounded-full bg-gray-5" />
+
         <Text as="span" typography={'b2-heading'}>
           {username}
         </Text>
       </div>
+
       <button onClick={handleCloseSidebar}>
         <IconClose className="w-10 h-10" />
       </button>
--- src/shared/ui/gnb/sidebar-menu-list.tsx
+++ src/shared/ui/gnb/sidebar-menu-list.tsx
@@ -18,6 +18,7 @@ export default function SideBarMenuList({ text, children }: Prop) {
   return (
     <li className="flex items-center h-[3.625rem] p-5 rounded-lg gap-2 cursor-pointer hover:bg-primary-30">
       {children}
+
       <a href="/">
         <Text as="span" typography={'b2-heading'}>
           {text}
