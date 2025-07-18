11ffe3b3f6641d66ed23b061ecbf4c0f9bc0ef12
yujin <kingyujin56@gmail.com>
2025-07-18 09:37:13 +0900

    feat: [#43] dropdown 컴포넌트 구현 (#51)
    * feat: 드롭다운 컴포넌트 구현(#43)
    
    * test:드롭다운 컴포넌트 테스트코드 작성(#43)
    
    * refactor: context 훅 분리(#43)
    
    * refactor:코드리뷰 반영(#43)
    
    * refactor: 파일명 컨벤션에 맞게 수정(#43)
    
    * docs: dropdown menu 설명 수정
    
    ---------
    
    Co-authored-by: apple <apple@appleui-MacBookAir.local>

---
+++ src/shared/ui/dropdown/dropdown-menu-item.tsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import { useDropdownContext } from './hooks/use-dropdown-context'
+/**
+ * @description 드롭다운 메뉴 리스트
+ *
+ * @param onClick 메뉴 클릭하면 발생할 이벤트, 없다면 드롭다운 닫힘
+ *
+ * @returns
+ *
+ * @example
+ * <Dropdown.Item onClick={() => alert('프로필 이동')}>
+ *   프로필
+ * </Dropdown.Item>
+ *
+ */
+
+interface Props {
+  children: React.ReactNode
+  onClick?: () => void
+}
+
+export default function DropDownMenuItem({ children, onClick }: Props) {
+  const { handleCloseDropdown } = useDropdownContext()
+
+  const handleClick = () => {
+    if (onClick) onClick()
+    handleCloseDropdown()
+  }
+
+  const handleKeyDown = (e: React.KeyboardEvent) => {
+    if (e.key === 'Enter' || e.key === ' ') {
+      e.preventDefault()
+      handleClick()
+    }
+  }
+
+  return (
+    <li onClick={handleClick} onKeyDown={handleKeyDown} tabIndex={0} className="w-full cursor-pointer">
+      {children}
+    </li>
+  )
+}
