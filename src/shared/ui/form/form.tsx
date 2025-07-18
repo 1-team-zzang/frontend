70abc763de08fb38bab259aef736b59236f78e44
최민경 <mxkxx@naver.com>
2025-07-18 17:08:32 +0900

    refactor: [#97] form 컴포넌트 Prop 수정 (#98)
    * refactor: form prop으로 useForm에서 반환된 객체 전달하도록 수정
    
    * refactor: 폼 컴포넌트 수정으로 인한 테스트 코드 수정
    
    * refactor: 폼 컴포넌트 수정으로 인한 input 테스트 코드 수정

--- src/shared/ui/form/form.tsx
+++ src/shared/ui/form/form.tsx
@@ -1,13 +1,12 @@
-import { useForm, FormProvider, type UseFormProps, type Resolver, type FieldValues } from 'react-hook-form'
+import { FormProvider, type UseFormProps, type FieldValues, type UseFormReturn } from 'react-hook-form'
 
 import { cn } from '../../utils'
 
 import type { ReactNode } from 'react'
 
 interface Props<T extends FieldValues> extends UseFormProps<T> {
-  resolver: Resolver<T>
   children: ReactNode
-
+  methods: UseFormReturn<T>
   onSubmit: (_data: T) => void
   className?: string
 }
@@ -15,26 +14,21 @@ interface Props<T extends FieldValues> extends UseFormProps<T> {
 /**
  * @template T - 폼 데이터 타입 (react-hook-form FieldValues 확장)
  * @param {object} props
- * @param {Resolver<T>} props.resolver - zod 스키마를 기반으로 생성한 react-hook-form 유효성 검사 함수
+ * @param {UseFormReturn<T>} methods - react-hook-form useForm 훅에서 반환된 메서드와 상태 객체
  * @param {ReactNode} props.children - 폼 내부에 렌더링할 자식 컴포넌트 (title, input, field 등)
  * @param {data: T} props.onSubmit - 폼 제출 시 실행되는 함수, 폼 데이터를 인자로 받음
  * @param {string | undefined} props.className - 추가적으로 적용할 CSS 클래스명
  */
 export default function Form<T extends FieldValues>({
-  resolver,
+  methods,
   onSubmit,
   children,
   className,
   ...restProps
 }: Props<T>) {
-  const methods = useForm<T>({
-    resolver,
-    ...restProps,
-  })
-
   return (
     <FormProvider {...methods}>
-      <form onSubmit={methods.handleSubmit(onSubmit)} className={cn('flex flex-col', className)}>
+      <form onSubmit={methods.handleSubmit(onSubmit)} className={cn('flex flex-col', className)} {...restProps}>
         {children}
       </form>
     </FormProvider>
