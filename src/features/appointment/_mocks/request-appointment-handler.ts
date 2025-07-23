1089f9b7edb3b02f39cdc54f6c0cb95a808a0329
d5ng <dong5ffice@gmail.com>
2025-07-23 15:36:08 +0900

    feat: 약속 신청 추가

---
+++ src/features/appointment/_mocks/request-appointment-handler.ts
@@ -0,0 +1,33 @@
+import { http, HttpResponse } from 'msw'
+
+import { validateRequiredFields } from '@/shared/utils'
+
+import type { RequestAppointmentPayload } from '../models/appointment.types'
+
+const REQUIRED_FIELDS = [
+  'title',
+  'startAt',
+  'endAt',
+  'requesterName',
+  'requesterEmail',
+  'isAllDay',
+  'receiverId',
+] as const
+
+export const requestAppointmentHandler = http.post('/api/appointments/requests', async ({ request }) => {
+  const requestPayload = (await request.json()) as RequestAppointmentPayload
+
+  if (!validateRequiredFields(requestPayload, REQUIRED_FIELDS)) {
+    return HttpResponse.json({
+      status: 'error',
+      code: 400,
+      message: '필수 필드가 누락되었습니다.',
+    })
+  }
+
+  return HttpResponse.json({
+    code: 200,
+    message: 'success',
+    data: {},
+  })
+})
