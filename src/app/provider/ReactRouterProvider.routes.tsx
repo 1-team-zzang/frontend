a570a4a3f00a55487b007807b8aee14c8f13096c
최민경 <mxkxx@naver.com>
2025-07-23 16:03:31 +0900

    feat: [#115] 로그인, 회원가입 페이지 (#116)
    * feat: auth 페이지 layout
    
    * feat: 로그인 페이지
    
    * feat: 회원가입 페이지
    
    * feat: auth 페이지 라우터 등록

--- src/app/provider/ReactRouterProvider.routes.tsx
+++ src/app/provider/ReactRouterProvider.routes.tsx
@@ -1,8 +1,26 @@
 import { createBrowserRouter } from 'react-router'
 
+import Authlayout from '@/pages/auth/auth-layout'
+import SigninPage from '@/pages/auth/signin/signin-page'
+import SignupPage from '@/pages/auth/signup/signup-page'
+
 export const router = createBrowserRouter([
   {
     path: '/',
     Component: () => <div>Hello World</div>,
+  },
+  {
+    path: '/auth',
+    Component: Authlayout,
+    children: [
+      {
+        path: 'signin',
+        Component: SigninPage,
+      },
+      {
+        path: 'signup',
+        Component: SignupPage,
+      },
+    ],
   },
 ])
