8ead40c213be7c2fef188a7b51fa341dab50ef65
mxkxx <mxkxx@naver.com>
2025-07-23 18:31:44 +0900

    refactor: 로그인 API에서 유저 정보를 반환하도록 수정

--- src/features/auth/signin/api/signin.API.ts
+++ src/features/auth/signin/api/signin.API.ts
@@ -1,12 +1,15 @@
 import axiosInstance from '@/shared/api/axios-instance'
 
 import type { SigninFormDataType } from '../model/signin.type'
+import type { User } from '@/entities/user/models/user.types'
 
-export async function postSignin(data: SigninFormDataType): Promise<void> {
+export async function postSignin(data: SigninFormDataType): Promise<User> {
   const res = await axiosInstance.post('/auth/login', data)
   const token = res.headers.authorization?.split(' ')[1]
 
   if (token) {
     localStorage.setItem('token', token)
   }
+
+  return res.data.data // 유저 정보만 반환
 }
