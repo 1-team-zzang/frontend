ca10200a2a116e50b17d8ad727fd9229ba72d598
d5ng <dong5ffice@gmail.com>
2025-07-23 15:29:14 +0900

    feat: 약속 types 추가

---
+++ src/entities/appointment/models/appointment.types.ts
@@ -0,0 +1,17 @@
+export type AppointmentStatus = 'REQUESTED' | 'ACCEPTED' | 'REJECTED'
+
+export interface Appointment {
+  appointmentId: number
+  title: string
+  content: string
+  startAt: string
+  endAt: string
+  requesterName: string
+  requesterEmail: string
+  appointmentStatus: AppointmentStatus
+  createdAt: string
+  modifiedAt: string
+  isAllDay: boolean
+  requesterId: number
+  receiverId: number
+}
