37e86b1d118d69a500a3663763b3b1042d78a409
humanpear <pibmaru@naver.com>
2025-07-23 18:46:03 +0900

    feat: Form 사용하도록 수정

--- src/pages/schedule-register/index.tsx
+++ src/pages/schedule-register/index.tsx
@@ -1,7 +1,10 @@
+import { zodResolver } from '@hookform/resolvers/zod'
 import { addHours, format, setMilliseconds, setMinutes, setSeconds } from 'date-fns'
 import { ko } from 'date-fns/locale'
 import { useState } from 'react'
+import { useForm } from 'react-hook-form'
 import { useNavigate } from 'react-router'
+import z from 'zod'
 
 import { IconArrowLeft } from '@/shared/assets/icons'
 import {
@@ -12,20 +15,38 @@ import {
   BottomSheetHeaderButton,
   BottomSheetContent,
 } from '@/shared/ui/bottom-sheet'
+import { Form, FormField } from '@/shared/ui/form'
 import { GNB } from '@/shared/ui/gnb'
+import { Input } from '@/shared/ui/input'
 import { Radio, RadioGroup } from '@/shared/ui/radio'
 import { Switch, SwitchTrigger } from '@/shared/ui/switch'
 import Text from '@/shared/ui/text/text'
+
+// form 설정
+const ScheduleSchema = z.object({
+  title: z.string().min(1, '일정 제목을 적어주세요'),
+})
+
+type ScheduleFormType = z.infer<typeof ScheduleSchema>
+
 export default function ScheduleRegister() {
+  const methods = useForm<ScheduleFormType>({
+    resolver: zodResolver(ScheduleSchema),
+    mode: 'onChange',
+    defaultValues: {
+      title: '',
+      repeat: 'none',
+    },
+  })
+  // eslint-disable-next-line unused-imports/no-unused-vars
+  const onSubmit = (value: ScheduleFormType) => {}
+
   const navigate = useNavigate()
 
   const onClickButton = () => {
     navigate(-1)
   }
   //이전 페이지로 갈 수 없을 경우도 만들어야함
-
-  //일정 제목
-  const [title, setTitle] = useState('')
 
   //시간 설정
   const now = new Date()
@@ -39,7 +60,7 @@ export default function ScheduleRegister() {
   //반복 바텀 시트
   const [isRepeatOpen, setIsRepeatOpen] = useState(false)
   // eslint-disable-next-line unused-imports/no-unused-vars
-  const [selecedRepeat, setSelectedRepeat] = useState('none')
+  const [selectedRepeat, setSelectedRepeat] = useState('none')
 
   //공개 바텀 시트
   const [isVisibleOpen, setIsVisibleOpen] = useState(false)
@@ -59,102 +80,98 @@ export default function ScheduleRegister() {
   //       },
   //     },
   //   })
+
   return (
     <div>
       <GNB />
-      <div className="flex justify-between items-center px-[1.25rem] py-[0.625rem]">
-        <IconArrowLeft onClick={onClickButton} className=" cursor-pointer" />
-        <Text typography={'h2-heading'}>일정 등록</Text>
-        <Text as="button" typography={'b2-normal'}>
-          저장
-        </Text>
-      </div>
-      <div className="flex flex-col px-4">
-        <div className="flex flex-col py-4 gap-2 border-b border-gray-10">
-          <Text typography={'b2-heading'}>일정 제목</Text>
-          <input
-            value={title}
-            className="bg-gray-1 text-gray-40 font-normal text-base leading-[1.6] tracking-[-0.04rem] rounded-s px-4 py-2.5 focus:outline-none"
-            onChange={(e) => setTitle(e.target.value)}
-            placeholder="일정 제목을 적어주세요"
-          />
-        </div>
-        <div className="flex py-4 justify-between items-center border-b border-gray-10">
-          <Text typography={'b2-heading'}>색 설정</Text>
-          <div className="rounded-full w-[1.75rem] h-[1.75rem] bg-primary-50" />
-        </div>
-        <div className="flex flex-col py-4 gap-4 border-b border-gray-10 font-normal text-base leading-[1.6] tracking-[-0.04rem]">
-          <div className="flex justify-between items-center">
-            <Text typography={'b2-heading'}>시작</Text>
-            <div className="flex gap-2">
-              <div>{format(startTime, 'yyyy.MM.dd (eee)', { locale: ko })}</div>
-              {!isChecked && <div>{format(startTime, 'a h:mm', { locale: ko })}</div>}
-            </div>
-          </div>
-          <div className="flex justify-between items-center">
-            <Text typography={'b2-heading'}>종료</Text>
-            <div className="flex gap-2">
-              <div>{format(endTime, 'yyyy.MM.dd (eee)', { locale: ko })}</div>
-              {!isChecked && <div>{format(endTime, 'a h:mm', { locale: ko })}</div>}
-            </div>
-          </div>
-        </div>
-        <div className="flex py-4 justify-between items-center border-b border-gray-10">
-          <Text typography={'b2-heading'}>하루 종일</Text>
-          <Switch checked={isChecked} onCheckedChange={setIsChecked}>
-            <SwitchTrigger />
-          </Switch>
-        </div>
-        <div className="flex py-4 justify-between items-center border-b border-gray-10">
-          <Text typography={'b2-heading'}>반복</Text>
-          <Text as="button" onClick={() => setIsRepeatOpen(true)} typography={'b2-normal'}>
-            {selecedRepeat}
+      <Form methods={methods} onSubmit={onSubmit}>
+        <div className="flex justify-between items-center px-[1.25rem] py-[0.625rem]">
+          <IconArrowLeft onClick={onClickButton} className=" cursor-pointer" />
+          <Text typography={'h2-heading'}>일정 등록</Text>
+          <Text as="button" type="submit" typography={'b2-normal'}>
+            저장
           </Text>
-          <BottomSheet defaultOpen={false} open={isRepeatOpen} onOpenChange={setIsRepeatOpen}>
-            <BottomSheetContainer>
-              <BottomSheetHeader>
-                <BottomSheetHeaderTitle>일정 등록</BottomSheetHeaderTitle>
-                <BottomSheetHeaderButton onClick={() => setIsRepeatOpen(false)}>완료</BottomSheetHeaderButton>
-              </BottomSheetHeader>
-              <BottomSheetContent>
-                <div>
-                  <RadioGroup className="flex flex-col gap-6" name="repeat" defaultValue={selecedRepeat}>
+        </div>
+        <div className="flex flex-col px-4">
+          <FormField name="title" className="flex flex-col py-4 gap-2 border-b border-gray-10">
+            <Text typography={'b2-heading'}>일정 제목</Text>
+            <Input placeholder="일정 제목을 적어주세요" />
+          </FormField>
+          <div className="flex py-4 justify-between items-center border-b border-gray-10">
+            <Text typography={'b2-heading'}>색 설정</Text>
+            <div className="rounded-full w-[1.75rem] h-[1.75rem] bg-primary-50" />
+          </div>
+          <div className="flex flex-col py-4 gap-4 border-b border-gray-10 font-normal text-base leading-[1.6] tracking-[-0.04rem]">
+            <div className="flex justify-between items-center">
+              <Text typography={'b2-heading'}>시작</Text>
+              <div className="flex gap-2">
+                <div>{format(startTime, 'yyyy.MM.dd (eee)', { locale: ko })}</div>
+                {!isChecked && <div>{format(startTime, 'a h:mm', { locale: ko })}</div>}
+              </div>
+            </div>
+            <div className="flex justify-between items-center">
+              <Text typography={'b2-heading'}>종료</Text>
+              <div className="flex gap-2">
+                <div>{format(endTime, 'yyyy.MM.dd (eee)', { locale: ko })}</div>
+                {!isChecked && <div>{format(endTime, 'a h:mm', { locale: ko })}</div>}
+              </div>
+            </div>
+          </div>
+          <div className="flex py-4 justify-between items-center border-b border-gray-10">
+            <Text typography={'b2-heading'}>하루 종일</Text>
+            <Switch checked={isChecked} onCheckedChange={setIsChecked}>
+              <SwitchTrigger />
+            </Switch>
+          </div>
+          <div className="flex py-4 justify-between items-center border-b border-gray-10">
+            <Text typography={'b2-heading'}>반복</Text>
+            <Text as="button" onClick={() => setIsRepeatOpen(true)} typography={'b2-normal'}>
+              {selectedRepeat}
+            </Text>
+            <BottomSheet defaultOpen={false} open={isRepeatOpen} onOpenChange={setIsRepeatOpen}>
+              <BottomSheetContainer>
+                <BottomSheetHeader>
+                  <BottomSheetHeaderTitle>일정 등록</BottomSheetHeaderTitle>
+                  <BottomSheetHeaderButton onClick={() => setIsRepeatOpen(false)}>완료</BottomSheetHeaderButton>
+                </BottomSheetHeader>
+                <BottomSheetContent>
+                  <RadioGroup className="flex flex-col gap-6" name="repeat" defaultValue={selectedRepeat}>
                     <Radio value="none">반복 안함</Radio>
                     <Radio value="day">일 단위 반복</Radio>
                     <Radio value="week">주 단위 반복</Radio>
                     <Radio value="month">월 단위 반복</Radio>
                     <Radio value="year">연 단위 반복</Radio>
                   </RadioGroup>
-                </div>
-              </BottomSheetContent>
-            </BottomSheetContainer>
-          </BottomSheet>
+                </BottomSheetContent>
+              </BottomSheetContainer>
+            </BottomSheet>
+          </div>
+          <div className="flex py-4 justify-between items-center border-b border-gray-10">
+            <Text typography={'b2-heading'}>공개</Text>
+            <Text as="button" onClick={() => setIsVisibleOpen(true)} typography={'b2-normal'}>
+              나만 보기
+            </Text>
+            <BottomSheet defaultOpen={false} open={isVisibleOpen} onOpenChange={setIsVisibleOpen}>
+              <BottomSheetContainer>
+                <BottomSheetHeader>
+                  <BottomSheetHeaderTitle>일정 등록</BottomSheetHeaderTitle>
+                  <BottomSheetHeaderButton onClick={() => setIsVisibleOpen(false)}>완료</BottomSheetHeaderButton>
+                </BottomSheetHeader>
+                <BottomSheetContent>
+                  <RadioGroup className="flex flex-col gap-6" name="visible" defaultValue={selectedVisible}>
+                    <Radio value="visible">전체 공개</Radio>
+                    <Radio value="invisible">나만 보기</Radio>
+                  </RadioGroup>
+                </BottomSheetContent>
+              </BottomSheetContainer>
+            </BottomSheet>
+          </div>
+          <div className="flex flex-col py-4 gap-2 border-b border-gray-10">
+            <Text typography={'b2-heading'}>일정 내용</Text>
+            <input placeholder="내용을 적어주세요." />
+          </div>
         </div>
-        <div className="flex py-4 justify-between items-center border-b border-gray-10">
-          <Text typography={'b2-heading'}>공개</Text>
-          <Text as="button" onClick={() => setIsVisibleOpen(true)} typography={'b2-normal'}>
-            나만 보기
-          </Text>
-          <BottomSheet defaultOpen={false} open={isVisibleOpen} onOpenChange={setIsVisibleOpen}>
-            <BottomSheetContainer>
-              <BottomSheetHeader>
-                <BottomSheetHeaderTitle>일정 등록</BottomSheetHeaderTitle>
-                <BottomSheetHeaderButton onClick={() => setIsVisibleOpen(false)}>완료</BottomSheetHeaderButton>
-              </BottomSheetHeader>
-              <BottomSheetContent>
-                <RadioGroup className="flex flex-col gap-6" name="visible" defaultValue={selectedVisible}>
-                  <Radio value="visible">전체 공개</Radio>
-                  <Radio value="invisible">나만 보기</Radio>
-                </RadioGroup>
-              </BottomSheetContent>
-            </BottomSheetContainer>
-          </BottomSheet>
-        </div>
-        <div className="flex flex-col py-4 gap-2 border-b border-gray-10">
-          <Text typography={'b2-heading'}>일정 내용</Text>
-          <input placeholder="내용을 적어주세요." />
-        </div>
-      </div>
+      </Form>
     </div>
   )
 }
