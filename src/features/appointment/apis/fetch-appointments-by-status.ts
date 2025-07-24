cf6aec2781dc9384f981ea002f4f8c2a6fc115c5
d5ng <dong5ffice@gmail.com>
2025-07-24 14:27:05 +0900

    refactor: fetchMyAppointmentsByStatus 타입 수정

--- src/features/appointment/apis/fetch-appointments-by-status.ts
+++ src/features/appointment/apis/fetch-appointments-by-status.ts
@@ -2,6 +2,6 @@ import axiosInstance from '@/shared/api/axios-instance'
 
 import type { MyAppointmentsByStatusParams } from '../models/appointment.types'
 
-export function fetchMyAppointmentsByStatus({ page, size, status }: MyAppointmentsByStatusParams) {
+export function fetchMyAppointmentsByStatus({ page, size, status }: MyAppointmentsByStatusParams & { page: number }) {
   return axiosInstance.get(`/appointments/requests?page=${page}&size=${size}&status=${status}`)
 }
