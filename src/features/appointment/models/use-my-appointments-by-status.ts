0c95da7adeecbc33cc3aac368284f6243cb8a141
d5ng <dong5ffice@gmail.com>
2025-07-24 14:28:31 +0900

    fix: 무한 스크롤 시 이전의 값 덮어씌어지는 문제 해결

--- src/features/appointment/models/use-my-appointments-by-status.ts
+++ src/features/appointment/models/use-my-appointments-by-status.ts
@@ -1,4 +1,4 @@
-import { useSuspenseQuery } from '@tanstack/react-query'
+import { useSuspenseInfiniteQuery } from '@tanstack/react-query'
 
 import { fetchMyAppointmentsByStatus } from '../apis/fetch-appointments-by-status'
 
@@ -6,9 +6,23 @@ import { appointmentsQuery } from './appointments.query'
 
 import type { MyAppointmentsByStatusParams } from './appointment.types'
 
-export function useMyAppointmentsByStatus({ page, size, status }: MyAppointmentsByStatusParams) {
-  return useSuspenseQuery({
-    queryKey: appointmentsQuery.myAppointmentsByStatus({ page, size, status }),
-    queryFn: () => fetchMyAppointmentsByStatus({ page, size, status }),
+export function useMyAppointmentsByStatus({ size, status }: MyAppointmentsByStatusParams) {
+  return useSuspenseInfiniteQuery({
+    queryKey: appointmentsQuery.myAppointmentsByStatus({ size, status }),
+    queryFn: ({ pageParam }) => fetchMyAppointmentsByStatus({ page: pageParam, size, status }),
+    getNextPageParam: (lastPage) => {
+      const currentPage = lastPage.data.data.page
+      const totalPages = lastPage.data.data.totalPages
+      return currentPage < totalPages ? currentPage + 1 : undefined
+    },
+    select: (data) => {
+      const appointments = data.pages.flatMap((page) => page.data.data.appointmentRequests)
+      return {
+        appointments,
+        page: data.pages[data.pages.length - 1]?.data.data.page,
+        totalPages: data.pages[data.pages.length - 1]?.data.data.totalPages,
+      }
+    },
+    initialPageParam: 1,
   })
 }
