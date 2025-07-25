016a244da20862a181c5b3f797d63a0d95f0b101
d5ng <dong5ffice@gmail.com>
2025-07-25 17:39:38 +0900

    refactor: apoointment-list-item 리팩토링

--- src/features/appointment/ui/appointment-list-item.tsx
+++ src/features/appointment/ui/appointment-list-item.tsx
@@ -1,9 +1,10 @@
 import { Link } from 'react-router'
 
 import Text from '@/shared/ui/text/text'
-import { formatDateToYMD, formatDateTimeWithDay } from '@/shared/utils'
+import { formatDateTimeWithDay } from '@/shared/utils'
 
 import AppointmentCard from './appointment-card'
+import AppointmentSender from './appointment-sender'
 
 import type { Appointment } from '@/entities/appointment/models'
 
@@ -13,19 +14,12 @@ export default function AppointmentListItem({
   startAt,
   endAt,
   inviteAt,
-}: Pick<Appointment, 'title' | 'requesterName' | 'startAt' | 'endAt' | 'inviteAt'>) {
+  id,
+}: Pick<Appointment, 'title' | 'requesterName' | 'startAt' | 'endAt' | 'inviteAt' | 'id'>) {
   return (
-    <Link to="/appointment/1">
-      <AppointmentCard>
-        <div className="flex justify-between items-center bg-gray-1 py-4 px-6">
-          <div className="flex items-center gap-[0.313rem]">
-            <Text typography="b2-heading">From</Text>
-            <Text typography="b2-normal">{requesterName}</Text>
-          </div>
-          <Text typography="b2-normal" className="text-gray-60">
-            {formatDateToYMD(inviteAt, 'dotted')}
-          </Text>
-        </div>
+    <Link to={`/appointment/requests/${id}`}>
+      <AppointmentCard className="p-0">
+        <AppointmentSender inviteAt={inviteAt} requesterName={requesterName} className="py-4 px-6 bg-gray-1" />
         <div className="flex flex-col items-center gap-[0.188rem] py-6">
           <Text typography="b2-heading">{title}</Text>
           <div className="flex items-center gap-[0.313rem]">
