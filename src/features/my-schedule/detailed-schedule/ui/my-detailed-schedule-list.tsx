8d2d49ccedc7d32dd7fa01b6eb739b36b2e65c8c
apple <apple@appleui-MacBookAir.local>
2025-07-25 21:31:31 +0900

    feat: 반복 텍스트 포멧

--- src/features/my-schedule/detailed-schedule/ui/my-detailed-schedule-list.tsx
+++ src/features/my-schedule/detailed-schedule/ui/my-detailed-schedule-list.tsx
@@ -1,9 +1,7 @@
-import { format } from 'date-fns'
 import { useNavigate, useParams } from 'react-router'
 
 import { formatScheduleTime } from '@/entities/utils/format-schedule-time'
-import { DetailedScheduleHeader, DetailedScheduleListCard } from '@/shared/ui/calendar'
-import { devLog } from '@/shared/utils/dev-log'
+import { DetailedScheduleListCard } from '@/shared/ui/calendar'
 
 import { useDateSchedules } from '../../hooks/use-date-schedules'
 
@@ -14,36 +12,30 @@ export default function MyDetailedScheduleList() {
   const { scheduleMap } = useDateSchedules()
 
   const list = date ? (scheduleMap[date] ?? []) : []
-  devLog('log', 'date-list', list)
-
-  const formattedDate = date ? format(new Date(date), 'M월 d일') : ''
 
   const onClick = (scheduleId: number) => {
-    navigate(`/my-calendar/schedule/${scheduleId}`)
+    navigate(`/my-calendar/date/${date}/schedule/${scheduleId}`)
   }
 
   return (
-    <div className="h-screen">
-      <DetailedScheduleHeader date={formattedDate} />
-      <div className="p-4 h-full">
-        {list.length === 0 ? (
-          <div className="text-center">아직 일정이 없어요</div>
-        ) : (
-          <div className="flex flex-col gap-4">
-            {list.map((card) => {
-              return (
-                <DetailedScheduleListCard
-                  onCardClick={() => onClick(card.scheduleId)}
-                  key={card.title}
-                  title={card.title}
-                  time={formatScheduleTime(card)}
-                  badgeColor={card.color}
-                />
-              )
-            })}
-          </div>
-        )}
-      </div>
+    <div>
+      {list.length === 0 ? (
+        <div className="text-center">아직 일정이 없어요</div>
+      ) : (
+        <div className="flex flex-col gap-4">
+          {list.map((card) => {
+            return (
+              <DetailedScheduleListCard
+                onCardClick={() => onClick(card.scheduleId)}
+                key={card.title}
+                title={card.title}
+                time={formatScheduleTime(card)}
+                badgeColor={card.color}
+              />
+            )
+          })}
+        </div>
+      )}
     </div>
   )
 }
--- src/features/my-schedule/detailed-schedule/ui/my-detailed-schedule.tsx
+++ src/features/my-schedule/detailed-schedule/ui/my-detailed-schedule.tsx
@@ -1,8 +1,8 @@
 import { useQuery } from '@tanstack/react-query'
 import { useParams } from 'react-router'
 
+import { getRepeatText } from '@/entities/utils/format-repeat-text'
 import DetailedScheduleCard from '@/shared/ui/calendar/ui/detailed-schedule-card'
-import { devLog } from '@/shared/utils/dev-log'
 
 import getDetailedSchedule from '../api/detailed-schedule.API'
 
@@ -13,14 +13,20 @@ export default function MyDetailedSchedule() {
     queryFn: () => getDetailedSchedule(scheduleId!),
     enabled: !!scheduleId,
   })
-  devLog('log', '세부일정', data)
+  const repeatText = getRepeatText({
+    repeatRule: data?.repeatRule ?? null,
+    repeatType: data?.repeatType ?? null,
+    repeatCount: data?.repeatCount ?? null,
+    repeatEndAt: data?.repeatEndAt ?? null,
+  })
+
   return data ? (
     <DetailedScheduleCard
       badgeColor={data.color}
       title={data.title}
       startDate={data.startAt}
       endDate={data.endAt}
-      repeat={data.repeatType ?? '없음'}
+      repeat={repeatText ?? ''}
     />
   ) : (
     <div>아직 일정이 없어요</div>
