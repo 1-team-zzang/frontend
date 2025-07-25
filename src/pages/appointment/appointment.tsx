c6b665713d085b413b03ec289116cf4ceef53067
d5ng <dong5ffice@gmail.com>
2025-07-25 17:56:02 +0900

    feat: 스켈레톤 ui 추가 및 적용

--- src/pages/appointment/appointment.tsx
+++ src/pages/appointment/appointment.tsx
@@ -3,12 +3,14 @@ import { ErrorBoundary } from 'react-error-boundary'
 
 import { AppointmentHeader, AppointmentList } from '@/features/appointment/ui'
 
+import AppointmentListSkeleton from './appointment-list-skeleton'
+
 export default function Appointment() {
   return (
     <main>
       <AppointmentHeader>내 약속</AppointmentHeader>
       <ErrorBoundary fallback={<div>Error</div>}>
-        <Suspense fallback={<div>Loading...</div>}>
+        <Suspense fallback={<AppointmentListSkeleton count={5} />}>
           <AppointmentList />
         </Suspense>
       </ErrorBoundary>
