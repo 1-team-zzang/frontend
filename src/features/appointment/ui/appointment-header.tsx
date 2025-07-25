eee9fd4abc6a72df10d9b5a4bbd4f78d05dc7675
d5ng <dong5ffice@gmail.com>
2025-07-25 15:01:57 +0900

    feat: appointment-header 추가

---
+++ src/features/appointment/ui/appointment-header.tsx
@@ -0,0 +1,9 @@
+import Text from '@/shared/ui/text/text'
+
+export default function AppointmentHeader() {
+  return (
+    <div className="border border-gray-10 py-2.5 text-center">
+      <Text typography="h2-heading">내 약속</Text>
+    </div>
+  )
+}
