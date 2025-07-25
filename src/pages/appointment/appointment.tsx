6430ddaac39adb3dcabb7e97a8b94c70229dc4a9
d5ng <dong5ffice@gmail.com>
2025-07-25 15:05:19 +0900

    feat: appointment 페이지 추가

--- src/pages/appointment/appointment.tsx
+++ src/pages/appointment/appointment.tsx
@@ -1,5 +1,17 @@
-import { MyAppointment } from '@/features/appointment/ui'
+import { Suspense } from 'react'
+import { ErrorBoundary } from 'react-error-boundary'
+
+import { AppointmentHeader, AppointmentList } from '@/features/appointment/ui'
 
 export default function Appointment() {
-  return <MyAppointment />
+  return (
+    <main>
+      <AppointmentHeader />
+      <ErrorBoundary fallback={<div>Error</div>}>
+        <Suspense fallback={<div>Loading...</div>}>
+          <AppointmentList />
+        </Suspense>
+      </ErrorBoundary>
+    </main>
+  )
 }
