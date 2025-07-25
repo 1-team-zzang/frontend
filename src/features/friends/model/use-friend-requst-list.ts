749702c0017fd21f7827982684437bb05591c0cf
mxkxx <mxkxx@naver.com>
2025-07-25 17:28:15 +0900

    feat: 친구 요청 목록을 가져오는 커스텀 useQuery  훅

---
+++ src/features/friends/model/use-friend-requst-list.ts
@@ -0,0 +1,20 @@
+import { useQuery } from '@tanstack/react-query'
+
+import { getFriendRequests } from '../api/friend.API'
+
+import { friendQueryKeys } from './friend.query'
+
+function useFriendRequestList(page: number, size = 10) {
+  const { data, isLoading, error, refetch } = useQuery({
+    queryKey: friendQueryKeys.requestList(page),
+    queryFn: () => getFriendRequests(page, size),
+  })
+
+  const friendRequestList = data?.friendRequests ?? []
+  const totalPages = data?.totalPages
+  const friendRequestCount = friendRequestList.length
+
+  return { friendRequestList, friendRequestCount, totalPages, error, isLoading, refetch }
+}
+
+export default useFriendRequestList
