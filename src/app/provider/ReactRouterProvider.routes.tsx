172ecd7d54f864dc225ed24dcc95d65a8b3907d9
apple <apple@appleui-MacBookAir.local>
2025-07-25 21:16:12 +0900

    feat: 내캘린더 라우터

--- src/app/provider/ReactRouterProvider.routes.tsx
+++ src/app/provider/ReactRouterProvider.routes.tsx
@@ -3,11 +3,22 @@ import { createBrowserRouter } from 'react-router'
 import Authlayout from '@/pages/auth/auth-layout'
 import SigninPage from '@/pages/auth/signin/signin-page'
 import SignupPage from '@/pages/auth/signup/signup-page'
+import DetailedScheduleLayout from '@/pages/home/my-detailed-schedule/my-detailed-schedule-layout'
+import MyDetailedScheduleListPage from '@/pages/home/my-detailed-schedule/my-detailed-schedule-list-page'
+import MyDetailedSchedulePage from '@/pages/home/my-detailed-schedule/my-detailed-schedule-page'
 
 export const router = createBrowserRouter([
   {
     path: '/',
-    Component: () => <div>Hello World</div>,
+    Component: () => <div>Hello world</div>,
+  },
+  {
+    path: '/my-calendar',
+    Component: DetailedScheduleLayout,
+    children: [
+      { path: 'date/:date', Component: MyDetailedScheduleListPage },
+      { path: 'date/:date/schedule/:scheduleId', Component: MyDetailedSchedulePage },
+    ],
   },
   {
     path: '/auth',
