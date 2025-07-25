cf4f20253e66cc803e377df4565537532ae5fc37
mxkxx <mxkxx@naver.com>
2025-07-25 17:25:42 +0900

    feat: searchType 및 친구 관련된 response 타입 정의

--- src/features/friends/model/friend-list.types.ts
+++ src/features/friends/model/friend-list.types.ts
@@ -1,7 +1,13 @@
-import type { Friend } from '@/entities/friends/models/friend.types'
+import type { Friend, FriendRequest, FriendUser } from '@/entities/friends/models/friend.types'
 
-export interface FriendListResponse {
-  friends: Friend[]
+export type FriendSearchType = 'EMAIL' | 'NAME'
+
+// NOTE 공통으로 빼면 좋을 것 같은데 어디로 빼야할지 ...?
+export type PaginatedResponse<T, K extends string> = {
   page: number
   totalPages: number
-}
+} & Record<K, T[]>
+
+export type FriendListResponse = PaginatedResponse<Friend, 'friends'>
+export type FriendUsersResponse = PaginatedResponse<FriendUser, 'users'>
+export type FriendRequestsResponse = PaginatedResponse<FriendRequest, 'friendRequests'>
