3fab17abeddd6a25f55d90462233b7f58ce22ff1
mxkxx <mxkxx@naver.com>
2025-07-25 17:17:26 +0900

    refactor: 친구 목록 가져오는 api함수에 파라미터 추가

--- src/features/friends/api/friend.API.ts
+++ src/features/friends/api/friend.API.ts
@@ -2,12 +2,16 @@ import axiosInstance from '@/shared/api/axios-instance'
 
 import type { FriendListResponse } from '../model/friend-list.types'
 
-export async function getFriends(): Promise<FriendListResponse> {
-  const res = await axiosInstance.get('/friends')
+// GET 친구 목록
+export async function getFriends(page = 1, size = 10): Promise<FriendListResponse> {
+  const res = await axiosInstance.get('/friends', {
+    params: { page, size },
+  })
 
-  return res.data
+  return res.data.data
 }
 
+// DELETE 친구 삭제
 export async function deleteFriend(friendRequestId: number): Promise<void> {
   await axiosInstance.delete(`/friends/${friendRequestId}`)
 }
