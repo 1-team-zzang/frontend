902e95e2138ddb1af3299eb6ec33753a0d7dc826
mxkxx <mxkxx@naver.com>
2025-07-25 09:41:22 +0900

    feat: 친구 삭제 시 쿼리 무효화 추가

--- src/features/friends/model/use-friend-delete-mutation.tsx
+++ src/features/friends/model/use-friend-delete-mutation.tsx
@@ -1,14 +1,18 @@
-import { useMutation } from '@tanstack/react-query'
+import { useMutation, useQueryClient } from '@tanstack/react-query'
 
 import { devLog } from '@/shared/utils/dev-log'
 
 import { deleteFriend } from '../api/friend.API'
 
+import { friendQueryKeys } from './query-key'
+
 function useFriendDeleteMutation() {
+  const queryClient = useQueryClient()
   return useMutation({
     mutationFn: (friendRequestId: number) => deleteFriend(friendRequestId),
     onSuccess: () => {
       devLog('log', '성공적으로 친구 삭제')
+      queryClient.invalidateQueries({ queryKey: friendQueryKeys.all })
     },
     onError: (error) => {
       devLog('error', '친구 삭제 실패', error.message)
