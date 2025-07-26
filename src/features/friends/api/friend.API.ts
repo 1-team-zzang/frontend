bdaeb505248275fd65a706fa9e659b6d61411151
mxkxx <mxkxx@naver.com>
2025-07-26 19:44:05 +0900

    feat: 친구 요청에 대해 수락/거절하는 API

--- src/features/friends/api/friend.API.ts
+++ src/features/friends/api/friend.API.ts
@@ -51,3 +51,11 @@ export async function getFriendRequests(page = 1, size = 10): Promise<FriendRequ
 export async function postFriendRequest(friendId: number): Promise<void> {
   await axiosInstance.post('/friends/requests', { friendId })
 }
+
+// PUT 친구 요청 수락/거절
+export async function putFriendRequest(status: 'ACCEPT' | 'REJECT', friendRequestId: number): Promise<void> {
+  await axiosInstance.put('/friends/requests', {
+    status,
+    friendRequestId,
+  })
+}
