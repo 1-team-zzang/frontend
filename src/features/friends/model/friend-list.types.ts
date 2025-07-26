dcfdbde1d2ae0919889064b765927337465e50b1
mxkxx <mxkxx@naver.com>
2025-07-26 20:58:19 +0900

    refactor: 코드래빗 리뷰에 따라 공통적으로 사용할 수 있는 타입 shared로 분리

--- src/features/friends/model/friend-list.types.ts
+++ src/features/friends/model/friend-list.types.ts
@@ -1,12 +1,7 @@
 import type { Friend, FriendRequest, FriendUser } from '@/entities/friends/models/friend.types'
+import type { PaginatedResponse } from '@/shared/types/api.types'
 
 export type FriendSearchType = 'EMAIL' | 'NAME'
-
-// NOTE 공통으로 빼면 좋을 것 같은데 어디로 빼야할지 ...?
-export type PaginatedResponse<T, K extends string> = {
-  page: number
-  totalPages: number
-} & Record<K, T[]>
 
 export type FriendListResponse = PaginatedResponse<Friend, 'friends'>
 export type FriendUsersResponse = PaginatedResponse<FriendUser, 'users'>
