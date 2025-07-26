4c18879c6a6064b37719df80fbdf1fcfa6ed1aa5
d5ng <dong5ffice@gmail.com>
2025-07-26 11:31:30 +0900

    feat: 선언적 코드 작성 (Error, Suspense)

--- src/pages/appointment/appointment.tsx
+++ src/pages/appointment/appointment.tsx
@@ -1,15 +1,26 @@
+import { useQueryErrorResetBoundary } from '@tanstack/react-query'
 import { Suspense } from 'react'
 import { ErrorBoundary } from 'react-error-boundary'
+import { useNavigate } from 'react-router'
 
 import { AppointmentHeader, AppointmentList } from '@/features/appointment/ui'
+import { ErrorFallback } from '@/shared/ui/error-fallback'
 
 import AppointmentListSkeleton from './appointment-list-skeleton'
 
 export default function Appointment() {
+  const navigate = useNavigate()
+  const { reset } = useQueryErrorResetBoundary()
+
   return (
     <main>
       <AppointmentHeader>내 약속</AppointmentHeader>
-      <ErrorBoundary fallback={<div>Error</div>}>
+      <ErrorBoundary
+        onReset={reset}
+        FallbackComponent={({ error, resetErrorBoundary }) => (
+          <ErrorFallback error={error} resetErrorBoundary={resetErrorBoundary} navigate={navigate} />
+        )}
+      >
         <Suspense fallback={<AppointmentListSkeleton count={5} />}>
           <AppointmentList />
         </Suspense>
