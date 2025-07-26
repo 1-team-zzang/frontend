96284b8b3205dbaae3487db031628fddf1e7f6d6
mxkxx <mxkxx@naver.com>
2025-07-26 19:45:57 +0900

    feat: 친구 목록에 대한 쿼리키 추가

--- src/features/friends/model/friend.query.ts
+++ src/features/friends/model/friend.query.ts
@@ -2,6 +2,7 @@ import type { FriendSearchType } from './friend-list.types'
 
 export const friendQueryKeys = {
   all: ['friends'] as const,
+  listAll: () => [...friendQueryKeys.all, 'list'],
   list: (page: number) => [...friendQueryKeys.all, 'list', page] as const,
   search: (searchType: FriendSearchType, query: string, page: number) =>
     [...friendQueryKeys.all, 'search', searchType, query, page] as const,
