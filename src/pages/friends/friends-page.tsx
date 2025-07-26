a065b823e92cb6aba85935cbdb5c3ae4a7de255c
mxkxx <mxkxx@naver.com>
2025-07-26 20:27:43 +0900

    refactor: 친구 목록 페이지 컴포넌트 변경

--- src/pages/friends/friends-page.tsx
+++ src/pages/friends/friends-page.tsx
@@ -1,18 +1,16 @@
+import AddFriendButton from '@/features/friends/ui/add-friend-button'
 import FriendList from '@/features/friends/ui/friend-list'
-import Button from '@/shared/ui/button/button.tsx'
+import FriendRequestListButton from '@/features/friends/ui/friend-request-list-button'
 import Header from '@/shared/ui/header/header'
 
 export default function FriendsPage() {
-  const cnt: number = 0
   return (
     <>
-      <Header leftButton={<div />} rightButton={<button>추가</button>}>
+      <Header leftButton={<div />} rightButton={<AddFriendButton />}>
         캘메이트
       </Header>
       <div className="px-5 py-3">
-        <Button intent="outlined" className="w-full" disabled={cnt === 0}>
-          {cnt > 0 ? `대기 중 초대 ${cnt}` : '친구 요청 없음'}
-        </Button>
+        <FriendRequestListButton />
       </div>
       <FriendList />
     </>
