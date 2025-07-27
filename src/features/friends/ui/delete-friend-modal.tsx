9218cd415a85be18b6608217a35226e3a35f2570
mxkxx <mxkxx@naver.com>
2025-07-28 01:35:49 +0900

    feat: 친구 삭제 시 모달이 뜨도록 모달 구현

---
+++ src/features/friends/ui/delete-friend-modal.tsx
@@ -0,0 +1,61 @@
+import { useState } from 'react'
+
+import { IconClose } from '@/shared/assets/icons'
+import Button from '@/shared/ui/button/button.tsx'
+import {
+  Modal,
+  ModalContent,
+  ModalDescription,
+  ModalOverlay,
+  ModalPortal,
+  ModalTitle,
+  ModalTrigger,
+} from '@/shared/ui/modal'
+
+import { useFriendDeleteMutation } from '../model'
+
+interface Props {
+  name: string
+  friendRequestId: number
+}
+
+export default function DeleteFriendModal({ name, friendRequestId }: Props) {
+  const [isOpen, setIsOpen] = useState<boolean>(false)
+
+  const handleClose = () => {
+    setIsOpen(false)
+  }
+
+  const friendDeleteMutation = useFriendDeleteMutation()
+
+  const handleDelete = () => {
+    friendDeleteMutation.mutate(friendRequestId, {
+      onSuccess: () => setIsOpen(false),
+    })
+  }
+  return (
+    <Modal open={isOpen} onOpenChange={setIsOpen}>
+      <ModalTrigger>
+        <IconClose width={24} height={24} className="text-system-warning" />
+      </ModalTrigger>
+      <ModalPortal>
+        <ModalOverlay />
+        <ModalContent className="text-center">
+          <ModalTitle>친구 삭제</ModalTitle>
+          <ModalDescription className="mt-2 mb-4">
+            <b>{name}</b> 친구를 삭제하시겠습니까? <br />
+            삭제하시면 친구의 캘린더를 확인할 수 없습니다.
+          </ModalDescription>
+          <div className="w-full flex gap-2">
+            <Button intent="outlined" className="flex-1" onClick={handleClose}>
+              취소
+            </Button>
+            <Button intent="solid" className="flex-1" onClick={handleDelete}>
+              삭제
+            </Button>
+          </div>
+        </ModalContent>
+      </ModalPortal>
+    </Modal>
+  )
+}
