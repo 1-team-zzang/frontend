24ec67a5e36f0d987e2ba2e4f0beaa75deaf3e55
apple <apple@appleui-MacBookAir.local>
2025-07-27 16:15:28 +0900

    feat: 비공개 일정 클릭시 모달 생성

--- src/shared/ui/calendar/ui/detailed-schedule-header.tsx
+++ src/shared/ui/calendar/ui/detailed-schedule-header.tsx
@@ -11,7 +11,7 @@ export default function DetailedScheduleHeader({ date }: Props) {
   const navigate = useNavigate()
 
   return (
-    <div className="flex justify-between items-center border-t border-b border-gray-10 py-[0.625rem] px-[1.25rem] z-fixed">
+    <div className="bg-white flex justify-between items-center border-t border-b border-gray-10 py-[0.625rem] px-[1.25rem] z-fixed">
       <IconArrowLeft className="size-4 cursor-pointer" onClick={() => navigate(-1)} />
       <Text as="h1" typography="h2-heading">
         {date}
--- src/shared/ui/calendar/ui/detailed-schedule-list-card.tsx
+++ src/shared/ui/calendar/ui/detailed-schedule-list-card.tsx
@@ -4,12 +4,13 @@ import Text from '@/shared/ui/text/text'
 import DetailedScheduleColorBadge from './detailed-schedule-color-badge'
 
 import type { ColorType } from '@/entities/schedule'
+import type { ReactNode } from 'react'
 
 interface Props {
   title: string
   time: string
   badgeColor: ColorType
-  onCardClick: () => void
+  onCardClick: () => void | ReactNode
 }
 
 export default function DetailedScheduleListCard({ title, time, badgeColor, onCardClick }: Props) {
