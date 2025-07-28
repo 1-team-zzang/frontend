fed6c61f45918866ab013ce3c732560994c842e6
mxkxx <mxkxx@naver.com>
2025-07-28 10:10:37 +0900

    fix: build 에러 수정

--- src/app/mocks/handlers.ts
+++
@@ -1,4 +0,0 @@
-import { friendsHandlers } from '@/features/friends/_mocks'
-import { myScheduleHandlers } from '@/features/my-schedule/_mocks'
-
-export const handlers = [...friendsHandlers, ...myScheduleHandlers]
