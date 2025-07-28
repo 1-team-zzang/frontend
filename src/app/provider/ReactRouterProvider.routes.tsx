f68ae5424b1f4753677a0f44a23597094fe340a8
mxkxx <mxkxx@naver.com>
2025-07-28 11:41:28 +0900

    refactor: index 파일 추가

--- src/app/provider/ReactRouterProvider.routes.tsx
+++ src/app/provider/ReactRouterProvider.routes.tsx
@@ -2,21 +2,23 @@ import { createBrowserRouter } from 'react-router'
 
 import { MyDetailedSchedule, MyDetailedScheduleList } from '@/features/my-schedule'
 import { ShareDetailedSchedule, ShareDetailedScheduleList } from '@/features/share-schedule'
-import AppointmentDetailPage from '@/pages/appointment/appointment-detail-page'
-import AppointmentListPage from '@/pages/appointment/appointment-list-page'
-import Authlayout from '@/pages/auth/auth-layout'
-import MySettingsPage from '@/pages/auth/settings/my-settings-page'
-import SigninPage from '@/pages/auth/signin/signin-page'
-import SignupPage from '@/pages/auth/signup/signup-page'
-import DetailedScheduleLayout from '@/pages/detailed-schedule/detailed-schedule-layout'
-import FriendDetailedSchedulePage from '@/pages/detailed-schedule/friend-detailed-schedule/friend-detailed-schedule'
-import FriendDetailedScheduleListPage from '@/pages/detailed-schedule/friend-detailed-schedule/friend-detailed-schedule-list-page'
-import FriendCalendarPage from '@/pages/friend-calendar/friend-calendar-page'
-import FriendsPage from '@/pages/friends/friends-page'
-import Home from '@/pages/home/home'
-import MainLayout from '@/pages/main-layout'
-import PrivateRoute from '@/pages/private-route'
-import ShareCalendarPage from '@/pages/share-calendar/share-calendar-page'
+import {
+  AppointmentDetailPage,
+  AppointmentListPage,
+  AuthLayout,
+  DetailedScheduleLayout,
+  FriendCalendarPage,
+  FriendDetailedScheduleListPage,
+  FriendDetailedSchedulePage,
+  FriendsPage,
+  Home,
+  MainLayout,
+  MySettingsPage,
+  PrivateRoute,
+  ShareCalendarPage,
+  SigninPage,
+  SignupPage,
+} from '@/pages'
 
 export const router = createBrowserRouter([
   {
@@ -117,7 +119,7 @@ export const router = createBrowserRouter([
 
   {
     path: '/auth',
-    Component: Authlayout,
+    Component: AuthLayout,
     children: [
       {
         path: 'signin',
