4f309df3638409b3a9d7aeed6b0359bea981f25c
mxkxx <mxkxx@naver.com>
2025-07-28 09:39:24 +0900

    refactor: 에러바운더리 제거 및 mutation 에러 시 에러메시지 출력

--- src/features/auth/signin/ui/email-signin-modal.tsx
+++ src/features/auth/signin/ui/email-signin-modal.tsx
@@ -1,10 +1,5 @@
-import { ErrorBoundary } from 'react-error-boundary'
-
 import { ModalContent, ModalOverlay, ModalPortal, Modal, ModalTitle } from '@/shared/ui/modal'
 import Text from '@/shared/ui/text/text'
-import { devLog } from '@/shared/utils/dev-log'
-
-import { getAuthErrorMessage } from '../../model/get-auth-error-message'
 
 import SigninForm from './signin-form'
 
@@ -18,37 +13,29 @@ interface Props {
 
 export default function EmailSigninModal({ isOpen, setSwitchModal, setClose }: Props) {
   return (
-    <ErrorBoundary
-      fallbackRender={({ error }) => {
-        // TODO 에러 문구를 토스트로
-        // NOTE 근데 비밀번호 틀리면 401 권한없음 에러가 온다 이게맞나?
-        devLog('log', 'error', getAuthErrorMessage(error.response.data.errorCode))
+    <Modal open={isOpen} onOpenChange={setClose}>
+      <ModalPortal>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalTitle className="text-center">로그인</ModalTitle>
 
-        return null
-      }}
-    >
-      <Modal open={isOpen} onOpenChange={setClose}>
-        <ModalPortal>
-          <ModalOverlay />
-          <ModalContent>
-            <ModalTitle className="text-center">로그인</ModalTitle>
-            <SigninForm onSigninSuccess={() => setSwitchModal(null)} />
-            {/* TODO 컴포넌트화
+          <SigninForm onSigninSuccess={() => setSwitchModal(null)} />
+
+          {/* TODO 컴포넌트화
             NOTE 모달 어떻게 열지 */}
-            <Text typography="b2-normal" className="text-gray-80 flex gap-1 items-center justify-center mt-6">
-              <span>캘픽이 처음이신가요?</span>
-              <Text
-                as="button"
-                typography="b2-heading"
-                onClick={() => setSwitchModal('Signup')}
-                className="text-primary-80 underline decoration-solid decoration-2 decoration-skip-ink underline-offset-4"
-              >
-                회원가입
-              </Text>
+          <Text typography="b2-normal" className="text-gray-80 flex gap-1 items-center justify-center mt-6">
+            <span>캘픽이 처음이신가요?</span>
+            <Text
+              as="button"
+              typography="b2-heading"
+              onClick={() => setSwitchModal('Signup')}
+              className="text-primary-80 underline decoration-solid decoration-2 decoration-skip-ink underline-offset-4"
+            >
+              회원가입
             </Text>
-          </ModalContent>
-        </ModalPortal>
-      </Modal>
-    </ErrorBoundary>
+          </Text>
+        </ModalContent>
+      </ModalPortal>
+    </Modal>
   )
 }
--- src/features/auth/signup/ui/signup-modal.tsx
+++ src/features/auth/signup/ui/signup-modal.tsx
@@ -1,10 +1,5 @@
-import { ErrorBoundary } from 'react-error-boundary'
-
 import { ModalContent, ModalOverlay, ModalPortal, Modal, ModalTitle } from '@/shared/ui/modal'
 import Text from '@/shared/ui/text/text'
-import { devLog } from '@/shared/utils/dev-log'
-
-import { getAuthErrorMessage } from '../../model/get-auth-error-message'
 
 import SignupForm from './signup-form'
 
@@ -18,38 +13,29 @@ interface Props {
 
 export default function SignupModal({ isSignupOpen, setSwitchModal, setClose }: Props) {
   return (
-    <ErrorBoundary
-      fallbackRender={({ error }) => {
-        // TODO 에러 문구를 토스트로
-        devLog('log', 'error', getAuthErrorMessage(error.response.data.errorCode))
+    <Modal open={isSignupOpen} onOpenChange={setClose}>
+      <ModalPortal>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalTitle className="text-center">회원가입</ModalTitle>
+          <SignupForm onSignupSuccess={() => setSwitchModal('EmailLogin')} />
 
-        return null
-      }}
-    >
-      <Modal open={isSignupOpen} onOpenChange={setClose}>
-        <ModalPortal>
-          <ModalOverlay />
-          <ModalContent>
-            <ModalTitle className="text-center">회원가입</ModalTitle>
-            <SignupForm onSignupSuccess={() => setSwitchModal('EmailLogin')} />
+          <Text typography="b2-normal" className="text-gray-80 flex gap-1 items-center justify-center mt-6">
+            <span>이미 회원이신가요?</span>
 
-            <Text typography="b2-normal" className="text-gray-80 flex gap-1 items-center justify-center mt-6">
-              <span>이미 회원이신가요?</span>
-
-              {/* TODO 컴포넌트화
+            {/* TODO 컴포넌트화
           NOTE 모달 어떻게 열지 */}
-              <Text
-                as="button"
-                typography="b2-heading"
-                onClick={() => setSwitchModal('EmailLogin')}
-                className="text-primary-80 underline decoration-solid decoration-2 decoration-skip-ink underline-offset-4"
-              >
-                로그인
-              </Text>
+            <Text
+              as="button"
+              typography="b2-heading"
+              onClick={() => setSwitchModal('EmailLogin')}
+              className="text-primary-80 underline decoration-solid decoration-2 decoration-skip-ink underline-offset-4"
+            >
+              로그인
             </Text>
-          </ModalContent>
-        </ModalPortal>
-      </Modal>
-    </ErrorBoundary>
+          </Text>
+        </ModalContent>
+      </ModalPortal>
+    </Modal>
   )
 }
