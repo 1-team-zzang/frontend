b22d6c5bf87a216ab149e449d2663964658f0f0e
apple <apple@appleui-MacBookAir.local>
2025-07-28 14:29:48 +0900

    fix: 빌드에러수정

--- src/shared/ui/calendar/util/handle-share-link-copy.ts
+++ src/shared/ui/calendar/util/handle-share-link-copy.ts
@@ -1,12 +1,10 @@
 import { devLog } from '@/shared/utils/dev-log'
 
-export default function handleShareLinkCopy(link: string, setCopied: (value: boolean) => void) {
+export default function handleShareLinkCopy(link: string) {
   return async () => {
     try {
       await navigator.clipboard.writeText(link)
-      setCopied(true)
       alert('링크가 복사되었습니다.')
-      setTimeout(() => setCopied(false), 2000)
     } catch (err) {
       devLog('error', '복사실패', err)
     }
