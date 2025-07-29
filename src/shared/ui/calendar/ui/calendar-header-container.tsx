42ace6419d1524a97c62ec34f0512ab4e50f19c6
apple <apple@appleui-MacBookAir.local>
2025-07-29 11:40:02 +0900

    feat: 친구캘린더 뒤로가기 버튼 추가

--- src/shared/ui/calendar/ui/calendar-header-container.tsx
+++ src/shared/ui/calendar/ui/calendar-header-container.tsx
@@ -14,5 +14,7 @@ import type { ReactNode } from 'react'
  */
 
 export default function CalendarHeader({ children }: { children: ReactNode }) {
-  return <div className="sticky top-0 z-10 bg-white flex justify-between px-5 py-[0.625rem]">{children}</div>
+  return (
+    <div className="sticky top-0 z-10 bg-white flex items-center justify-between px-5 py-[0.625rem]">{children}</div>
+  )
 }
