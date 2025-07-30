061c371c24020eead758762c8ff7c5796f1ac4aa
apple <apple@appleui-MacBookAir.local>
2025-07-30 19:54:06 +0900

    fix: 연속일정 버그 수정

--- src/entities/schedule/models/get-group-by-date.ts
+++ src/entities/schedule/models/get-group-by-date.ts
@@ -1,15 +1,32 @@
-import { format } from 'date-fns'
+import { format, eachDayOfInterval } from 'date-fns'
 
 import type { Schedule } from './schedule.types'
 
 export default function groupByDate(schedules: Schedule[]) {
   const map: Record<string, Schedule[]> = {}
+
   for (const schedule of schedules) {
-    const date = format(new Date(schedule.startAt), 'yyyy-MM-dd')
-    if (!map[date]) {
-      map[date] = []
+    // 반복 일정이면 startAt만 사용
+    if (schedule.isRepeated) {
+      const date = format(new Date(schedule.startAt), 'yyyy-MM-dd')
+      if (!map[date]) {
+        map[date] = []
+      }
+      map[date].push(schedule)
+    } else {
+      const start = new Date(schedule.startAt)
+      const end = new Date(schedule.endAt)
+      const days = eachDayOfInterval({ start, end })
+
+      for (const day of days) {
+        const dateStr = format(day, 'yyyy-MM-dd')
+        if (!map[dateStr]) {
+          map[dateStr] = []
+        }
+        map[dateStr].push(schedule)
+      }
     }
-    map[date].push(schedule)
   }
+
   return map
 }
