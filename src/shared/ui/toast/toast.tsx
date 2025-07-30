8653025ba8b50b1daf4402dfd3b552e5c4924c50
apple <apple@appleui-MacBookAir.local>
2025-07-30 15:01:48 +0900

    feat: toast 포탈 추가

--- src/shared/ui/toast/toast.tsx
+++ src/shared/ui/toast/toast.tsx
@@ -2,6 +2,7 @@
 import { useEffect } from 'react'
 
 import ToastContent from './toast-content'
+import ToastPortal from './toast-portal'
 import { useToastStore } from './use-toast-store'
 
 export default function Toast() {
@@ -19,10 +20,12 @@ export default function Toast() {
   }, [toasts, removeToast])
 
   return (
-    <div className="fixed bottom-4 translate-x-1/2 z-toast space-y-1">
-      {toasts.map((toast) => (
-        <ToastContent key={toast.id}>{toast.message}</ToastContent>
-      ))}
-    </div>
+    <ToastPortal>
+      <div className="fixed bottom-4 translate-x-1/2 z-toast space-y-1">
+        {toasts.map((toast) => (
+          <ToastContent key={toast.id}>{toast.message}</ToastContent>
+        ))}
+      </div>
+    </ToastPortal>
   )
 }
