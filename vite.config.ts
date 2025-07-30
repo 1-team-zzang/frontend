a8b0d0a6dc3f5a71e5d19199b2eac87f9b707082
최민경 <mxkxx@naver.com>
2025-07-30 09:29:42 +0900

    chore: [#202] 로컬 https 환경 구성 (#203)
    * docs: .pem 파일들 gitignore에 추가
    
    * chore: vite config 파일 수정

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,35 @@
+/* eslint-disable indent */
+
+import fs from 'fs'
+
 import react from '@vitejs/plugin-react'
+import { loadEnv, type ConfigEnv } from 'vite'
 import svgr from 'vite-plugin-svgr'
 import tsconfigPaths from 'vite-tsconfig-paths'
-import { defineConfig } from 'vitest/config'
+import { defineConfig as defineTestConfig } from 'vitest/config'
 
-export default defineConfig({
-  plugins: [react(), tsconfigPaths(), svgr({ include: '**/*.svg?react' })],
-  test: {
-    environment: 'jsdom',
-    setupFiles: ['./setup.test.tsx'],
-    include: ['src/**/*.test.{ts,tsx}'],
-  },
-})
+export default ({ mode }: ConfigEnv) => {
+  // eslint-disable-next-line no-undef
+  const env = loadEnv(mode, process.cwd())
+
+  const isDevelop: boolean = env.VITE_DEVELOP === 'true'
+
+  return defineTestConfig({
+    plugins: [react(), tsconfigPaths(), svgr({ include: '**/*.svg?react' })],
+
+    test: {
+      environment: 'jsdom',
+      setupFiles: ['./setup.test.tsx'],
+      include: ['src/**/*.test.{ts,tsx}'],
+    },
+
+    server: {
+      https: isDevelop
+        ? {
+            key: fs.readFileSync('localhost-key.pem'),
+            cert: fs.readFileSync('localhost.pem'),
+          }
+        : undefined,
+    },
+  })
+}
