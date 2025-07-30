0cc7aff01d3b170089b3115e51bc2a9eb3e45305
apple <apple@appleui-MacBookAir.local>
2025-07-30 09:28:15 +0900

    refactor: 일정리스트 페이지 높이 수정

--- src/pages/detailed-schedule/detailed-schedule-layout.tsx
+++ src/pages/detailed-schedule/detailed-schedule-layout.tsx
@@ -10,7 +10,7 @@ export default function DetailedScheduleLayout() {
   const formattedDate = date ? format(new Date(date), 'M월 d일') : ''
 
   return (
-    <main className="bg-[#f8f8f8] overflow-y-auto h-[calc(100vh-127px)]">
+    <main className="bg-[#f8f8f8] overflow-y-auto h-[calc(100vh-57px)] scrollbar-hide">
       <DetailedScheduleHeader date={formattedDate} />
       <div className="p-4">
         <Outlet />
