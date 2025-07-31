825d373e2cf00a43213e4bf769f87ffa7db24b15
mxkxx <mxkxx@naver.com>
2025-07-31 18:22:20 +0900

    feat: 프로필 수정 mutation 훅

---
+++ src/features/profile/image/model/use-upload-profile-image.ts
@@ -0,0 +1,21 @@
+import { useMutation } from '@tanstack/react-query'
+
+import { useUserStore } from '@/entities/user/models/use-user-store'
+import { toast } from '@/shared/ui/toast'
+
+import { putUserProfile } from '../../api/profile.API'
+
+import type { UserProfileRequestData } from '../../types/profile.types'
+
+export default function useUploadProfileImageMutation() {
+  return useMutation({
+    mutationFn: (data: UserProfileRequestData) => putUserProfile(data),
+    onSuccess: (updatedUser) => {
+      toast.success('프로필이 변경되었습니다')
+      useUserStore.setState({ user: updatedUser })
+    },
+    onError: () => {
+      toast.error('프로필 변경에 실패했습니다. 다시 시도해주세요')
+    },
+  })
+}
