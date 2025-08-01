b98082a79c3cad80ef86c3eb8a06f4cf36d7026d
apple <apple@appleui-MacBookAir.local>
2025-08-01 17:48:39 +0900

    refactor: 공유캘린더 무한스크롤 적용

--- src/features/share-schedule/share-calendar/share-calendar.tsx
+++ src/features/share-schedule/share-calendar/share-calendar.tsx
@@ -1,42 +1,50 @@
 import { format } from 'date-fns'
 import { useNavigate, useParams } from 'react-router'
 
+import isPastDate from '@/entities/utils/is-paste-date'
 import { IconInvite } from '@/shared/assets/icons'
-import { DateCellContent } from '@/shared/ui/calendar'
-import CalendarUI from '@/widget/ui/calendar-ui'
+import {
+  AddScheduleButton,
+  Calendar,
+  HeaderButton,
+  HeaderContainer,
+  HearderMonthLabel,
+  InfiniteCalendar,
+  RenderScheduleBadges,
+} from '@/shared/ui/calendar'
+import { toast } from '@/shared/ui/toast'
 
-import AddScheduleButton from '../../../shared/ui/calendar/ui/add-schedule-button'
-import isPastDate from '../../../shared/ui/calendar/util/is-past-date'
 import { useShareSchedule } from '../hooks/use-share-schedule'
 
 export default function ShareCalendar() {
-  const { scheduleMap } = useShareSchedule()
   const navigate = useNavigate()
   const { userId } = useParams<{ userId: string }>()
-
-  const handleDateClick = (date: Date, navigate: ReturnType<typeof useNavigate>) => {
+  const { scheduleMap } = useShareSchedule()
+  const onDateClick = (date: Date) => {
     if (isPastDate(date)) {
-      alert('오늘 이전 날짜는 선택할 수 없습니다')
-      return
+      toast.error('오늘 이전 날짜는 선택할 수 없습니다')
     }
     const dateStr = format(date, 'yyyy-MM-dd')
     navigate(`/share/${userId}/detailed-schedule/date/${dateStr}`)
   }
 
   const goToCreateAppointment = () => {
-    alert('약속 신청하기')
+    alert('약속신청하기')
   }
 
   return (
-    <CalendarUI
-      disablePastDateStyling={false}
-      renderDateCellContent={(date) => DateCellContent({ scheduleMap, date, isPast: isPastDate(date) })}
-      onDateClick={(date) => handleDateClick(date, navigate)}
-      isShareCalendar={true}
-    >
+    <Calendar onDateClick={onDateClick}>
+      <HeaderContainer>
+        <div className="size-4" />
+        <HearderMonthLabel />
+        <HeaderButton>오늘</HeaderButton>
+      </HeaderContainer>
+      <InfiniteCalendar disablePrev={true}>
+        {(date) => <RenderScheduleBadges date={date} scheduleMap={scheduleMap} />}
+      </InfiniteCalendar>
       <AddScheduleButton onClick={goToCreateAppointment}>
         <IconInvite />
       </AddScheduleButton>
-    </CalendarUI>
+    </Calendar>
   )
 }
