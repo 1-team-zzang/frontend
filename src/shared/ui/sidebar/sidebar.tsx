64d23eef6720f27b5ead24434b0ecac2f8a45eb4
mxkxx <mxkxx@naver.com>
2025-08-03 22:50:28 +0900

    style: 사이드바 fixed에서 absolute로 수정

--- src/shared/ui/sidebar/sidebar.tsx
+++ src/shared/ui/sidebar/sidebar.tsx
@@ -39,7 +39,7 @@ export default function SideBar({ children }: Props) {
       exit="closed"
       variants={sidebarVariants}
       transition={{ type: 'tween', duration: 0.3 }}
-      className="w-[16rem] h-full fixed top-0 right-0 flex flex-col justify-between bg-white p-[0.625rem] z-side-bar rounded-s-lg"
+      className="w-[16rem] h-full absolute top-0 right-0 flex flex-col justify-between bg-white p-[0.625rem] z-side-bar rounded-s-lg"
     >
       {children}
     </motion.nav>
