8a8562b5a2d02c508184bda28991dafba0744fde
apple <apple@appleui-MacBookAir.local>
2025-08-03 18:43:39 +0900

    feat: toast 성공,실패 디자인 수정

--- src/shared/ui/toast/toast-content.tsx
+++ src/shared/ui/toast/toast-content.tsx
@@ -1,37 +1,33 @@
-import { cva, type VariantProps } from 'class-variance-authority'
 import { motion } from 'framer-motion'
+
+import { IconToastFail, IconToastSuccess } from '@/shared/assets/icons'
 
 import Text from '../text/text'
 
 import type { ReactNode } from 'react'
 
-const toastVariants = cva('w-fit py-[0.625rem] px-4 bg-gray-100 text-white rounded-lg z-toast whitespace-nowrap ', {
-  variants: {
-    type: {
-      success: '',
-      error: '',
-    },
-  },
-  defaultVariants: {
-    type: 'success',
-  },
-})
-interface Props extends VariantProps<typeof toastVariants> {
+type ToastType = 'success' | 'error'
+
+interface Props {
+  type: ToastType
   children: ReactNode
 }
 
 export default function ToastContent({ type, children }: Props) {
   return (
     <motion.div
-      className={toastVariants({ type })}
+      className="w-fit py-[0.625rem] px-4 bg-gray-100 text-white rounded-lg z-toast whitespace-nowrap"
       initial={{ scale: 0.95, y: 30, opacity: 0 }}
-      animate={{ scale: 1, y: 0, opacity: 0.8 }}
+      animate={{ scale: 1, y: 0, opacity: 0.9 }}
       exit={{ scale: 0.9, y: 30, opacity: 0 }}
       transition={{ duration: 0.5, ease: 'easeOut' }}
     >
-      <Text as="span" typography="b2-normal">
-        {children}
-      </Text>
+      <div className="flex gap-[0.625rem] items-center">
+        {type === 'success' ? <IconToastSuccess /> : <IconToastFail />}
+        <Text as="span" typography="b2-normal">
+          {children}
+        </Text>
+      </div>
     </motion.div>
   )
 }
