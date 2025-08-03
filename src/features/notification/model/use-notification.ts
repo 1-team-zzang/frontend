a626a32a12861053ef42caf652769164218a06d9
mxkxx <mxkxx@naver.com>
2025-08-04 01:08:54 +0900

    feat: 알림 쿼리키 정의 및 알림 리스트 무한스크롤 훅 구현

---
+++ src/features/notification/model/use-notification.ts
@@ -0,0 +1,26 @@
+import { useSuspenseInfiniteQuery } from '@tanstack/react-query'
+
+import { getNotification } from '../api/notification.API'
+
+import { notificationQueryKeys } from './notification.query'
+
+export default function useNotification(size = 10) {
+  return useSuspenseInfiniteQuery({
+    queryKey: notificationQueryKeys.list(),
+    queryFn: ({ pageParam }) => getNotification(pageParam, size),
+    getNextPageParam: (lastPage) => {
+      const currentPage = lastPage.page
+      const totalPages = lastPage.totalPages
+      return currentPage < totalPages ? currentPage + 1 : undefined
+    },
+    select: (data) => {
+      const notifications = data.pages.flatMap((page) => page.notifications)
+      return {
+        notifications,
+        page: data.pages[data.pages.length - 1]?.page,
+        totalPages: data.pages[data.pages.length - 1]?.totalPages,
+      }
+    },
+    initialPageParam: 1,
+  })
+}
