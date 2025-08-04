f55d38eebc22dde7da6b48477dce57252fd86a79
최민경 <mxkxx@naver.com>
2025-08-04 15:28:51 +0900

    refactor: [#244] shared 폴더 내부 index 파일 export 구조 통일 (#246)
    * refactor: shared 내부 파일들은 /shared 경로에서 import 하도록 index문 수정
    
    * refactor: lib index 파일 default export로 수정
    
    * refactor: shared 내부의 책임을 분리하기 위해 shared/index 삭제
    
    * refactor: index 컴포넌트 추가 및 네이밍 규칙에 맞는 컴포넌트명으로 수정

--- src/shared/utils/index.ts
+++ src/shared/utils/index.ts
@@ -1,11 +1,15 @@
 export * from './cn'
 export * from './create-context-scope'
+
 export * from './Slot'
 export * from './Slottable'
 
 // Date
-export * from './isUndefined'
 export * from './formatDateToYMD'
 export * from './format-date-time-with-day'
 
 export * from './format-relative-date'
+
+export * from './dev-log'
+export * from './is-function'
+export * from './is-undefined'
