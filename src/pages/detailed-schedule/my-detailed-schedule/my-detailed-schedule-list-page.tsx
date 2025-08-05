2647b1e701f4eb376400d7911bba06b8e14ab968
apple <apple@appleui-MacBookAir.local>
2025-08-05 14:29:51 +0900

    refactor: import문 수정

--- src/pages/detailed-schedule/my-detailed-schedule/my-detailed-schedule-list-page.tsx
+++ src/pages/detailed-schedule/my-detailed-schedule/my-detailed-schedule-list-page.tsx
@@ -1,6 +1,6 @@
 import { Suspense } from 'react'
 
-import { MyDetailedScheduleList } from '@/features/my-schedule'
+import { MyDetailedScheduleList } from '@/features/my-schedule/ui'
 
 import DetailedScheduleListSkeleton from '../detailed-schedule-list-skeleton'
 
