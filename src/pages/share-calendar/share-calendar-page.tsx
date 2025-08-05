fbbadbbad0e24d5d417b6d7c9af4d3afcbb56c61
apple <apple@appleui-MacBookAir.local>
2025-08-05 17:35:09 +0900

    refactor: 배럴패턴적용

--- src/pages/share-calendar/share-calendar-page.tsx
+++ src/pages/share-calendar/share-calendar-page.tsx
@@ -1,6 +1,6 @@
 import { Outlet } from 'react-router'
 
-import ShareCalendar from '@/features/share-schedule/ui/share-calendar'
+import { ShareCalendar } from '@/features/share-schedule/ui'
 
 export default function ShareCalendarPage() {
   return (
