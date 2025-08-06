0524a69e15c59ba7a2c3bf0dc267b546a996e3f7
mxkxx <mxkxx@naver.com>
2025-08-06 11:07:48 +0900

    feat: 뒤로 가기 버튼 공용 컴포넌트 구현

--- src/shared/ui/index.ts
+++ src/shared/ui/index.ts
@@ -1,5 +1,5 @@
 export * from './bottom-sheet'
-export { default as Button } from './button/button.tsx'
+export * from './button'
 export * from './calendar'
 export * from './checkbox'
 export * from './dropdown'
