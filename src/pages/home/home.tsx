8d05cbb75975ce04ada6144bc3e7c8359c2154ed
apple <apple@appleui-MacBookAir.local>
2025-08-07 13:30:19 +0900

    fix: 모달 조건문 수정

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import { Outlet } from 'react-router'
 
 import { useUserStore } from '@/entities/user'
-import { LoginSelectModal } from '@/features/auth/signin/ui'
+import { EmailSigninModal, LoginSelectModal } from '@/features/auth/signin/ui'
 import { SignupModal } from '@/features/auth/signup/ui'
 import { MyCalendar } from '@/features/my-schedule/ui'
 import { IconCalendarArrowLeft } from '@/shared/assets'
@@ -15,7 +15,7 @@ export default function Home() {
   const user = useUserStore((state) => state.user)
 
   const [isOpen, setIsOpen] = useState(!user) // 로그인 안 되어 있으면 기본값 true
-  const [switchModal, setSwitchModal] = useState<AuthModalType>('EmailLogin')
+  const [switchModal, setSwitchModal] = useState<AuthModalType>('LoginSelect')
   const onDateClick = () => {
     if (!user) {
       setSwitchModal('EmailLogin')
@@ -38,7 +38,8 @@ export default function Home() {
             <HeaderButton>오늘</HeaderButton>
           </HeaderContainer>
           <InfiniteCalendar />
-          {switchModal === 'EmailLogin' ? (
+
+          {switchModal === 'LoginSelect' && (
             <LoginSelectModal
               isOpen={isOpen}
               setClose={setIsOpen}
@@ -49,9 +50,24 @@ export default function Home() {
                 }
               }}
             />
-          ) : (
+          )}
+
+          {switchModal === 'Signup' && (
             <SignupModal
               isSignupOpen={isOpen}
+              setClose={setIsOpen}
+              setSwitchModal={(mode) => {
+                setSwitchModal(mode)
+                if (mode === 'EmailLogin') {
+                  setIsOpen(true)
+                }
+              }}
+            />
+          )}
+
+          {switchModal === 'EmailLogin' && (
+            <EmailSigninModal
+              isOpen={isOpen}
               setClose={setIsOpen}
               setSwitchModal={(mode) => {
                 setSwitchModal(mode)
