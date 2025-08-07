4b67dc63fcb33e655590aec139cf259fe2529471
apple <apple@appleui-MacBookAir.local>
2025-08-07 16:22:03 +0900

    feat: detailed-schedule 스토리북작성

---
+++ src/shared/ui/detailed-schedule/detailed-schedule-date.stories.tsx
@@ -0,0 +1,22 @@
+import DetailedScheduleDate from './detailed-schedule-date'
+
+import type { Meta, StoryObj } from '@storybook/react-vite'
+
+const meta: Meta<typeof DetailedScheduleDate> = {
+  title: 'shared/detailed-schedule/detailed-schedule-date',
+  component: DetailedScheduleDate,
+  tags: ['autodocs'],
+  argTypes: {},
+}
+export default meta
+
+type Story = StoryObj<typeof DetailedScheduleDate>
+
+export const Default: Story = {
+  args: {
+    date: '2025-08-01',
+    weekday: '금',
+    AmPm: '오후',
+    time: '01:00',
+  },
+}
---
+++ src/shared/ui/detailed-schedule/detailed-schedule-header.stories.tsx
@@ -0,0 +1,28 @@
+import { MemoryRouter } from 'react-router'
+
+import DetailedScheduleHeader from './detailed-schedule-header'
+
+import type { Meta, StoryObj } from '@storybook/react-vite'
+
+const meta: Meta<typeof DetailedScheduleHeader> = {
+  title: 'shared/detailed-schedule/detailed-schedule-hearder',
+  component: DetailedScheduleHeader,
+  tags: ['autodocs'],
+  decorators: [
+    (Story) => (
+      <MemoryRouter>
+        <Story />
+      </MemoryRouter>
+    ),
+  ],
+  argTypes: {},
+}
+export default meta
+
+type Story = StoryObj<typeof DetailedScheduleHeader>
+
+export const Default: Story = {
+  args: {
+    date: '8월1일',
+  },
+}
