211a79028ea716991731ab82ccd71a59b897dd01
yujin <kingyujin56@gmail.com>
2025-08-07 11:33:48 +0900

    refactor: [#272] entities/schedule 베럴패턴적용 (#280)
    * refactor: 베럴패턴적용
    
    * refactor: 코드리뷰반영
    
    ---------
    
    Co-authored-by: apple <apple@appleui-MacBookAir.local>

--- src/entities/schedule/models/detailed-schedule.API.ts
+++ src/entities/schedule/models/detailed-schedule.API.ts
@@ -1,6 +1,6 @@
 import axiosInstance from '@/shared/api/axios-instance'
 
-import type { Schedule } from '@/entities/schedule'
+import type { Schedule } from '@/entities/schedule/models'
 
 export async function getDetailedSchedule(scheduleId: string): Promise<Schedule> {
   const res = await axiosInstance(`/schedules/${scheduleId}`)
