457d044dc18b0e358dd4461cde5b0ca8f68aff50
mxkxx <mxkxx@naver.com>
2025-08-07 13:40:38 +0900

    feat: page 이동하는 prompt 컴포넌트

--- src/features/auth/signup/ui/signup-modal.tsx
+++ src/features/auth/signup/ui/signup-modal.tsx
@@ -1,5 +1,7 @@
 import { ModalContent, ModalOverlay, ModalPortal, Modal, ModalTitle, Text } from '@/shared/ui'
 
+import { PromptSwitch } from '../../ui'
+
 import SignupForm from './signup-form'
 
 import type { AuthModalType } from '../../types'
@@ -22,16 +24,7 @@ export default function SignupModal({ isSignupOpen, setSwitchModal, setClose }: 
           <Text typography="b2-normal" className="text-gray-80 flex gap-1 items-center justify-center mt-6">
             <span>이미 회원이신가요?</span>
 
-            {/* TODO 컴포넌트화
-          NOTE 모달 어떻게 열지 */}
-            <Text
-              as="button"
-              typography="b2-heading"
-              onClick={() => setSwitchModal('EmailLogin')}
-              className="text-primary-80 underline decoration-solid decoration-2 decoration-skip-ink underline-offset-4"
-            >
-              로그인
-            </Text>
+            <PromptSwitch type="signup" to="/auth/signin" className="mt-6" />
           </Text>
         </ModalContent>
       </ModalPortal>
---
+++ src/features/auth/ui/index.ts
@@ -0,0 +1,1 @@
+export { default as PromptSwitch } from './prompt-switch'
