1cfff3829d3684aac27dfb545ffc122916f619e9
apple <apple@appleui-MacBookAir.local>
2025-08-08 09:36:14 +0900

    feat: 상세일정 리스트 해당일자의 데이터만 받아오도록 수정

--- src/features/my-schedule/hooks/use-date-schedules.ts
+++ src/features/my-schedule/hooks/use-date-schedules.ts
@@ -2,20 +2,25 @@ import { useSuspenseQuery } from '@tanstack/react-query'
 
 import { groupByDate, scheduleQueryKeys } from '@/entities/schedule/models'
 import { useUserStore } from '@/entities/user/models/use-user-store'
+import { devLog } from '@/shared/utils'
 
 import { getMySchedule } from '../api'
 
-export function useDateSchedules() {
+interface Props {
+  start: string
+  end: string
+}
+
+export function useDateSchedules({ start, end }: Props) {
   const userId = useUserStore((state) => state.user?.userId)
-  const start = '2025-01-01'
-  const end = '2025-12-31'
   const { data: schedules = [] } = useSuspenseQuery({
-    queryKey: scheduleQueryKeys.userSchedules(userId!),
+    queryKey: scheduleQueryKeys.userSchedulesList(userId!, start),
     queryFn: () => getMySchedule(start, end),
   })
 
   const scheduleMap = groupByDate(schedules)
-
+  devLog('log', 'scheduleMap', scheduleMap)
+  devLog('log', 'start, end', { start, end })
   return {
     scheduleMap,
   }
