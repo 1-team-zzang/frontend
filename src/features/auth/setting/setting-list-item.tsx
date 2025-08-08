a9a62cb85aa9bdba182bce3b6f931a8423c99d50
mxkxx <mxkxx@naver.com>
2025-08-08 16:52:50 +0900

    refactor: 카카오 로그인의 경우 비밀번호 변경 불가 및 페이지 접근 시 리다이렉트 처리

--- src/features/auth/setting/setting-list-item.tsx
+++ src/features/auth/setting/setting-list-item.tsx
@@ -1,25 +1,39 @@
 import { useNavigate } from 'react-router'
 
 import { IconAppointmentArrowLeft } from '@/shared/assets'
-import { Text } from '@/shared/ui'
+import { Text, Tooltip, TooltipMessage, TooltipTrigger } from '@/shared/ui'
+import { cn } from '@/shared/utils'
 
 import type { HTMLAttributes } from 'react'
 
 interface Props extends HTMLAttributes<HTMLDivElement> {
   href: string
+  isKaKaoLogin?: boolean
+  message?: string
 }
 
-function SettingListItem({ children, href }: Props) {
+function SettingListItem({ children, href, isKaKaoLogin, message }: Props) {
   const navigate = useNavigate()
 
   return (
     <div className="flex items-center justify-between px-6 py-4 border-b border-gray-10">
-      <Text as="span" typography="b2-normal">
+      <Text as="span" typography="b2-normal" className={cn(isKaKaoLogin && 'text-gray-40')}>
         {children}
       </Text>
-      <button onClick={() => navigate(href)}>
-        <IconAppointmentArrowLeft className="rotate-180" />
-      </button>
+      {isKaKaoLogin ? (
+        <Tooltip>
+          <TooltipTrigger className="cursor-not-allowed">
+            <IconAppointmentArrowLeft className="rotate-180" />
+          </TooltipTrigger>
+          <TooltipMessage position="top" arrowPosition="bottom">
+            {message}
+          </TooltipMessage>
+        </Tooltip>
+      ) : (
+        <button onClick={() => navigate(href)}>
+          <IconAppointmentArrowLeft className="rotate-180" />
+        </button>
+      )}
     </div>
   )
 }
