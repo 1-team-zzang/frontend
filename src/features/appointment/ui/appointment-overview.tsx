9245e200ee66814d51f127d87ca246f7934d718d
humanpear <pibmaru@naver.com>
2025-08-11 10:42:01 +0900

    feat: 피드백 수정

--- src/features/appointment/ui/appointment-overview.tsx
+++ src/features/appointment/ui/appointment-overview.tsx
@@ -7,8 +7,8 @@ export default function AppointmentOverview({ content }: Pick<AppointmentDetail,
     <>
       <div className="border-b border-[#D9D9D9]" />
       <div className="pt-5 flex flex-col gap-3">
-        {/* Image */}
-        <div className="w-full h-[200px] bg-gray-5 rounded-[0.625rem]" />
+        {/* Image
+        <div className="w-full h-[200px] bg-gray-5 rounded-[0.625rem]" /> */}
         {/* Content */}
         <Text typography="b2-normal">{content}</Text>
       </div>
