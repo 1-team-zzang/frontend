936308fc4980cbcb8c88e33110847fc1291db467
mxkxx <mxkxx@naver.com>
2025-08-11 14:50:14 +0900

    fix: 오타 수정

--- src/features/friends/model/index.ts
+++ src/features/friends/model/index.ts
@@ -4,7 +4,7 @@ export type * from './friend-list.types'
 
 export { default as useFriendDeleteMutation } from './use-friend-delete-mutation.ts'
 export { default as useFriendRequestMutation } from './use-friend-request-mutation'
-export { default as useFriendRequestList } from './use-friend-requst-list'
+export { default as useFriendRequestList } from './use-friend-request-list'
 export { default as useFriendsUsers } from './use-friends-users'
 export { default as useFriends } from './use-friends'
 export { default as useUpdateFriendRequestByStatusMutation } from './use-update-friend-request-by-status-mutation'
