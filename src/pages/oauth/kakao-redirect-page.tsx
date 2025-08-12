0d6da59a9ef7132868169b82d933724f533aaf2e
최민경 <mxkxx@naver.com>
2025-08-12 14:21:02 +0900

    refactor: [#337] 카카오 리다이렉트 페이지 로딩 처리 (#338)
    * feat: dots spinner 컴포넌트 구현
    
    * refactor: 카카오 리다이렉트 페이지 로딩 처리
    
    * docs: 주석 제거

--- src/pages/oauth/kakao-redirect-page.tsx
+++ src/pages/oauth/kakao-redirect-page.tsx
@@ -1,8 +1,8 @@
 import { useEffect } from 'react'
 import { useNavigate, useSearchParams } from 'react-router'
 
-import useKakaoSigninMutation from '@/features/oauth/kakao/model/use-kakao-signin-mutation'
-// 배럴 패턴으로 import 하면 에러 떠서 실제 경로로 import했어요
+import { useKakaoSigninMutation } from '@/features/oauth/kakao/model'
+import { Text, Dots } from '@/shared/ui'
 
 export default function KaKaoRedirectPage() {
   const [searchParams] = useSearchParams()
@@ -23,5 +23,13 @@ export default function KaKaoRedirectPage() {
   }, [code])
 
   // TODO  카카오 로그인 되면 수정 예정
-  return <div>카카오 로그인 처리 중입니다...</div>
+  return (
+    <main className="flex flex-col gap-4 items-center justify-center h-screen">
+      <Dots />
+      <Text as="p" typography="b2-heading" className="text-center">
+        카카오 로그인중입니다 <br />
+        잠시만 기다려주세요
+      </Text>
+    </main>
+  )
 }
